d0df616889258dc8fe5a2327489c11e1785fb7f1
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-03-29 11:03:32 -0500

    Clean up main run script

--- workflows/auen41_ff/py-keras-cooley.sh
+++ workflows/auen41_ff/py-keras-cooley.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 set -eu
 
+if [[ ${#} != 1 ]]
+then
+  echo "Requires data directory!"
+  exit 1
+fi
+
+DATA_DIRECTORY=$1
+
+THIS=$( cd $( dirname $0 ) ; /bin/pwd )
+
 # PYTHONPATH
 PP=
 PP+=/soft/analytics/conda/env/Candle_ML/lib/python2.7/site-packages:
 PP+=/soft/analytics/conda/env/Candle_ML/lib/python2.7:
-PP+=$HOME/pb-data/auen-intel-tflow
+PP+=$THIS
 # PYTHONHOME
 PH=/soft/analytics/conda/env/Candle_ML
 
-export MODE=cluster PROJECT=ExM QUEUE=default
-
-THIS=$PWD
-
-cd ~/pb-data
-export TURBINE_OUTPUT_ROOT=$PWD
-export TURBINE_OUTPUT_FORMAT=out-%Q
 ENVS="-e PYTHONHOME=$PH -e PYTHONPATH=$PP"
-swift-t -m cobalt $ENVS $THIS/py-keras.swift
+swift-t -m cobalt -s $THIS/settings.sh $ENVS \
+        $THIS/py-keras.swift --data-directory=$DATA_DIRECTORY
