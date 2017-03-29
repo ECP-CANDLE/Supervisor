19162aa4cab00ec2bafe50e4c7e60b5fe5ba218f
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-03-29 11:04:05 -0500

    Separate editable settings into separate file

---
+++ workflows/auen41_ff/settings.sh
@@ -0,0 +1,10 @@
+
+# SETTINGS.SH
+# This is a Bash-formatted file
+
+export MODE=cluster
+export PROJECT=Candle_ECP
+export QUEUE=default
+
+export TURBINE_OUTPUT_ROOT=$PWD
+export TURBINE_OUTPUT_FORMAT=out-%Q
