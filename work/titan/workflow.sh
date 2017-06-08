4e024d34e6b8fbfbd8a643eae9463cf9e155f44d
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-06-08 14:35:37 -0500

    New 2nd example

--- work/titan/workflow.sh
+++ work/titan/workflow.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eu
 
+if [[ ${#} != 1 ]]
+then
+  echo "Specify a Swift script!"
+  exit 1
+fi
+SCRIPT=$1
+
 LD_LIBRARY_PATH=/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3x/lib64/R/lib:/opt/java/jdk1.8.0_51/jre/lib/amd64/server:/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3/lib64/R/lib:/sw/xk6/curl/7.30.0/sles11.1_gnu4.3.4/lib:/opt/gcc/4.9.3/snos/lib64
 
 SWIFT=/lustre/atlas2/csc249/proj-shared/sfw/swift-t/stc/bin/swift-t
@@ -13,4 +20,4 @@ PROCS=4
 
 $SWIFT -m cray -n $PROCS \
        -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
-       workflow.swift
+       $SCRIPT
