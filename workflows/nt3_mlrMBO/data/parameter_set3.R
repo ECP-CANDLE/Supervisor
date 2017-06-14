e770a3fa366ba77f84fb4130300b236a30dcf5b3
pbalapra <pbalapra@anl.gov>
2017-06-14 20:20:52 +0000

    modifications to nt3

--- workflows/nt3_mlrMBO/data/parameter_set3.R
+++ workflows/nt3_mlrMBO/data/parameter_set3.R
@@ -2,9 +2,19 @@
 # the parameter names should match names of the arguments expected by
 # the benchmark
 param.set <- makeParamSet(
-  makeDiscreteParam("batch_size", values = c(32, 64, 128)),
-  makeIntegerParam("epochs", lower = 1, upper = 3),
-
+  makeDiscreteParam("batch_size", values = c(16, 32, 64, 128, 256, 512)),
+  makeIntegerParam("epochs", lower = 10, upper = 30),
+  makeDiscreteParam("activation", values = c("softmax","elu","softplus","softsign", 
+  				  	   	"relu", "tanh","sigmoid","hard_sigmoid",
+						"linear" )),
+  makeDiscreteParam("dense", values = c("500 100 50", "1000 500 100 50", 
+  			     	      	     "2000 1000 500 100 50")),
+  makeDiscreteParam("optimizer", values = c("adam", "sgd", "rmsprop","adagrad",
+  				 	  	    "adadelta","adamax","nadam")),
+  makeNumericParam("drop", lower = 0, upper = 0.5),
+  makeDiscreteParam("conv", values = c("50 50 50 50 50 1", "25 25 25 25 25 1", 
+  			    	     	   "10 10 1 5 5 1", "32 20 16 32 10 1"))
   ## DEBUG PARAMETERS: DON'T USE THESE IN PRODUCTION RUN
-  makeDiscreteParam("conv", values = c("32 20 16 32 10 1"))
+  ## makeDiscreteParam("conv", values = c("32 20 16 32 10 1"))
 )
+
