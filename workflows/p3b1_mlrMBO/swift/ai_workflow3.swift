02bb4642d85f1a283cdc169ac33cf2dbe5ded494
Nick Collier <ncollier@anl.gov>
2017-06-19 13:04:59 -0400

    Let write do the work

--- workflows/p3b1_mlrMBO/swift/ai_workflow3.swift
+++ workflows/p3b1_mlrMBO/swift/ai_workflow3.swift
@@ -36,7 +36,6 @@ app (file out, file err) run_model (file shfile, string param_file, string insta
 (string obj_result) obj(string params, string iter_indiv_id) {
   string outdir = "%s/run_%s" % (turbine_output, iter_indiv_id);
 
-  make_dir(outdir) =>
   string fname = "%s/params.json" % outdir =>
   file out <"%s/out.txt" % outdir> =>
   file params_file <fname> = write(params) =>
