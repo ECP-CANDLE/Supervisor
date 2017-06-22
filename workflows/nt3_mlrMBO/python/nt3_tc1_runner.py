7267116ea5744d508f76aeadf6e88848d4499bf8
Harry Yoo <hsyoo@anl.gov>
2017-06-21 21:30:27 -0500

    pass experiment id and run id to app runs

--- workflows/nt3_mlrMBO/python/nt3_tc1_runner.py
+++ workflows/nt3_mlrMBO/python/nt3_tc1_runner.py
@@ -59,9 +59,13 @@ if __name__ == '__main__':
     instance_directory = sys.argv[2]
     model_name = sys.argv[3]
     framework = sys.argv[4]
+    exp_id = sys.argv[5]
+    run_id = sys.argv[6]
     hyper_parameter_map = runner_utils.init(param_file, instance_directory,
                                             framework, 'save')
     hyper_parameter_map['model_name'] = model_name
+    hyper_parameter_map['experiment_id'] = exp_id
+    hyper_parameter_map['run_id'] = run_id
     # clear sys.argv so that argparse doesn't object
     sys.argv = ['nt3_tc1_runner']
     result = run(hyper_parameter_map)
--- workflows/nt3_mlrMBO/scripts/run_model.sh
+++ workflows/nt3_mlrMBO/scripts/run_model.sh
@@ -13,7 +13,7 @@ set -eu
 
 # !!! IF YOU CHANGE THE NUMBER OF ARGUMENTS PASSED TO THIS SCRIPT, YOU MUST
 # CHANGE THE TIMEOUT_ARG_INDEX !!!
-TIMEOUT_ARG_INDEX=6
+TIMEOUT_ARG_INDEX=8
 TIMEOUT=""
 if [[ $# ==  $TIMEOUT_ARG_INDEX ]]
 then
@@ -40,11 +40,13 @@ cd $instance_directory
 
 model_name=$4
 framework=$5
+exp_id=$6
+run_id=$7
 
-BENCHMARK_DIR=$emews_root/../../../Benchmarks/Pilot1/NT3:$emews_root/../../../Benchmarks/Pilot1/TC1
+BENCHMARK_DIR=$emews_root/../../../Benchmarks/common:$emews_root/../../../Benchmarks/Pilot1/NT3:$emews_root/../../../Benchmarks/Pilot1/TC1
 COMMON_DIR=$emews_root/../common/python
 export PYTHONPATH="$PYTHONPATH:$BENCHMARK_DIR:$COMMON_DIR"
-MODEL_CMD="python $emews_root/python/nt3_tc1_runner.py $param_file $instance_directory $model_name $framework"
+MODEL_CMD="python $emews_root/python/nt3_tc1_runner.py $param_file $instance_directory $model_name $framework $exp_id $run_id"
 
 # Turn bash error checking off. This is
 # required to properly handle the model execution return value
