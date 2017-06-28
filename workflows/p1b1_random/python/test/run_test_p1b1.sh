1e2c83e3456f59452666dcccb66fae11364c07d1
Rajeev Jain <rajeeja@gmail.com>
2017-06-28 05:02:42 -0500

    Update runner and fix output logging

--- workflows/p1b1_random/python/test/run_test_p1b1.sh
+++ workflows/p1b1_random/python/test/run_test_p1b1.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-P1B1_DIR=../../../../Benchmarks/Pilot1/P1B1
-export PYTHONPATH="$PWD/..:$P1B1_DIR"
+P1B1_DIR=../../../../../Benchmarks/Pilot1/P1B1
+export PYTHONPATH="$PWD/..:$P1B1_DIR:../../../common/python"
 echo $PYTHONPATH
 
 python test_p1b1.py
