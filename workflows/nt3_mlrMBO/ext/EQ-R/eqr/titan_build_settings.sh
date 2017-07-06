bbf4e4b6ca87f625d2d2eecf570d1abc64de8e58
brettin <brettin@anl.gov>
2017-07-06 16:26:56 -0400

    new file

---
+++ workflows/nt3_mlrMBO/ext/EQ-R/eqr/titan_build_settings.sh
@@ -0,0 +1,31 @@
+
+# TITAN BUILD SETTINGS
+
+R_HOME=/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3x/lib64/R
+R_INCLUDE=$R_HOME/include
+R_LIB=$R_HOME/lib
+R_INSIDE=$R_HOME/library/RInside
+RCPP=$R_HOME/library/Rcpp
+
+#system-wide tcl
+TCL=/lustre/atlas2/csc249/proj-shared/sfw/tcl-8.6.2
+TCL_INCLUDE=$TCL/include
+TCL_LIB=$TCL/lib
+TCL_LIBRARY=tcl8.6
+
+CPPFLAGS=""
+CPPFLAGS+="-I$TCL_INCLUDE "
+CPPFLAGS+="-I$R_INCLUDE "
+CPPFLAGS+="-I$RCPP/include "
+CPPFLAGS+="-I$R_INSIDE/include "
+CXXFLAGS=$CPPFLAGS
+
+LDFLAGS=""
+LDFLAGS+="-L$R_INSIDE/lib -lRInside "
+LDFLAGS+="-L$R_LIB -lR -lRblas "
+LDFLAGS+="-L$TCL_LIB -l$TCL_LIBRARY "
+LDFLAGS+="-Wl,-rpath -Wl,$TCL_LIB "
+LDFLAGS+="-Wl,-rpath -Wl,$R_LIB "
+LDFLAGS+="-Wl,-rpath -Wl,$R_INSIDE/lib"
+
+export CPPFLAGS CXXFLAGS LDFLAGS
