fe706bc69cf28868e86758ddaa3455f39d64abbf
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-07-06 14:56:14 -0500

    Refactor workflow p3b1_mlrmbo: bring in logging; make settings reusable

---
+++ workflows/common/sh/langs-theta.sh
@@ -0,0 +1,16 @@
+
+# LANGS Theta
+# Language settings for Theta (Swift, Python, R, Tcl, etc.)
+
+TCL=/home/wozniak/Public/sfw/theta/tcl-8.6.1
+export R=/home/wozniak/Public/sfw/theta/R-3.4.0/lib64/R
+export PY=/home/wozniak/Public/sfw/theta/Python-2.7.12
+export LD_LIBRARY_PATH=$PY/lib:$R/lib:$LD_LIBRARY_PATH
+COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
+PYTHONPATH=$EMEWS_PROJECT_ROOT/python:$BENCHMARK_DIR:$COMMON_DIR
+PYTHONHOME=/home/wozniak/Public/sfw/theta/Python-2.7.12
+
+# STC=/home/wozniak/Public/sfw/theta/swift-t-pyr/stc
+STC=/projects/Candle_ECP/swift/pyr/stc
+
+export PATH=$STC/bin:$TCL/bin:$PATH
