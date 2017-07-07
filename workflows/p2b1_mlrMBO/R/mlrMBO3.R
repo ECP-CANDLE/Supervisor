b1b329d49cf281c34f0a85d5dbea6a5808dcffe5
pbalapra <pbalapra@anl.gov>
2017-07-07 15:31:31 +0000

    minor edits to mlr scripts

--- workflows/p2b1_mlrMBO/R/mlrMBO3.R
+++ workflows/p2b1_mlrMBO/R/mlrMBO3.R
@@ -59,12 +59,13 @@ main_function <- function(max.budget = 110, max.iterations = 10, design.size=10,
 
 
   surr.rf = makeLearner("regr.randomForest", predict.type = "se")
-  ctrl = makeMBOControl(n.objectives = 1, propose.points = min(20, propose.points), 
-       	   			       	         impute.y.fun = function(x, y, opt.path, ...) .Machine$integer.max * 0.1 )
+  ctrl = makeMBOControl(n.objectives = 1,  save.on.disk.at = c(1,2,3,4,5), 
+       	 			     propose.points = min(20, propose.points), 
+       	   			     impute.y.fun = function(x, y, opt.path, ...) .Machine$integer.max * 0.1 )
   ctrl = setMBOControlInfill(ctrl, crit=makeMBOInfillCritCB(), interleave.random.points=max(0,propose.points-20))
   ctrl = setMBOControlMultiPoint(ctrl, method = "cb")
   ctrl = setMBOControlTermination(ctrl, max.evals = max.budget)
-  ctrl = setMBOControlTermination(ctrl, iters = max.iterations)
+  #ctrl = setMBOControlTermination(ctrl, iters = max.iterations)
 
   # ctrl = setMBOControlInfill(ctrl, crit =makeMBOInfillCritCB(), opt.focussearch.points = 500)
   design = generateDesign(n = design.size, par.set = getParamSet(obj.fun))
--- workflows/p2b1_mlrMBO/data/parameter_set3.R
+++ workflows/p2b1_mlrMBO/data/parameter_set3.R
@@ -3,14 +3,15 @@
 # the benchmark
 param.set <- makeParamSet(
   makeDiscreteParam("batch_size", values = c(16, 32, 64, 128, 256, 512)),
-  makeIntegerParam("epochs", lower = 1, upper = 2),
+  makeIntegerParam("epochs", lower = 5, upper = 50),
   makeDiscreteParam("activation", values = c("softmax","elu","softplus","softsign", 
   				  	   	"relu", "tanh","sigmoid","hard_sigmoid",
 						"linear")),
   makeDiscreteParam("optimizer", values = c("adam", "sgd", "rmsprop","adagrad",
   				 	  	    "adadelta")),
   makeNumericParam("learning_rate", lower = 0.001, upper = 0.2),
-  makeNumericParam("noise", lower = 0.001, upper = 0.5)
+  makeNumericParam("noise_factor", lower = 0.0, upper = 0.5),
+  makeNumericParam("weight_decay", lower = 0.000001, upper = 0.1)
   #makeDiscreteParam("num_hidden", values = c("512 256 128 64 32 16", "512 64")),
   #makeDiscreteParam("molecular_num_hidden", values = c("54 12", "32 16 8"))
   ## DEBUG PARAMETERS: DON'T USE THESE IN PRODUCTION RUN
