b4b7d3dcc4023652cf249107eb19b1313b82a800
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-07-31 16:58:20 -0500

    Clobber workflow.sh with titan_workflow.sh .  Now the site is an argument to workflow.sh (ref #22)

--- workflows/common/sh/utils.sh
+++ workflows/common/sh/utils.sh
@@ -18,6 +18,22 @@ log_path()
   eval echo \$$1 | tr : '\n' | nl
 }
 
+get_site()
+# Get site name (Titan, Theta, Cori, etc.)
+{
+  if (( ${#} < 1 ))
+  then
+    script_name=$(basename $0)
+    echo << EOF
+Usage: ${script_name} SITE EXPERIMENT_ID"
+where SITE is the computer name (titan, theta, cori) and
+      EXPERIMENT_ID will be the new directory under experiments
+EOF
+    exit 1
+  fi
+  export SITE=$1
+}
+
 get_expid()
 # Get Experiment IDentifier
 # EXPID is the name of the new directory under experiments/
@@ -54,4 +70,35 @@ EOF
   fi
   export TURBINE_OUTPUT=$EXPERIMENTS/$EXPID
   check_directory_exists
+}
+
+source_site()
+# Source a settings file for a specific SITE (titan, cori, theta)
+# Succeeds with warning message if file is not found
+{
+  if (( ${#} != 2 ))
+  then
+    echo "usage: source_site TOKEN SITE"
+    echo "where TOKEN is langs, modules, etc."
+    echo "  and SITE is titan, cori, theta, etc."
+    return 1
+  fi
+
+  TOKEN=$1
+  SITE=$2
+
+  if [[ ${WORKFLOWS_ROOT:-} == "" ]]
+  then
+    echo "Set WORKFLOWS_ROOT!"
+    return 1
+  fi
+
+  FILE=$WORKFLOWS_ROOT/common/sh/$TOKEN-$SITE.sh
+  if ! [[ -f $FILE ]]
+  then
+    echo "warning: no file: $FILE"
+    return 0
+  fi
+
+  source $FILE
 }
