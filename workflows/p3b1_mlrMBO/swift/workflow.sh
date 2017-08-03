615aa47e31f7713aa6ff59612eb9dbc77676364f
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-08-03 08:12:52 -0700

    Complete support for SITE abstraction; switch on SWIFT_IMPL

--- workflows/p3b1_mlrMBO/swift/workflow.sh
+++ workflows/p3b1_mlrMBO/swift/workflow.sh
@@ -7,12 +7,12 @@ set -eu
 # Autodetect this workflow directory
 export EMEWS_PROJECT_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
 WORKFLOWS_ROOT=$( cd $EMEWS_PROJECT_ROOT/.. ; /bin/pwd )
+BENCHMARKS_ROOT=$( cd $EMEWS_PROJECT_ROOT/../../../Benchmarks ; /bin/pwd)
+BENCHMARK_DIR=$BENCHMARKS_ROOT/Pilot3/P3B1
 
 # USER SETTINGS START
 
 # See README.md for more information
-
-BENCHMARK_DIR=$EMEWS_PROJECT_ROOT/../../../Benchmarks/Pilot3/P3B1
 
 # The number of MPI processes
 # Note that 2 processes are reserved for Swift/EMEMS
@@ -57,17 +57,35 @@ shift
 
 export TURBINE_JOBNAME="${EXPID}_job"
 
-source_site langs $SITE
+source_site modules $SITE
+source_site langs   $SITE
+source_site sched   $SITE
 
-CMD_LINE_ARGS=( $*
+# The Swift Implementation type: "app" or "py"
+# Selects the *.swift files to include
+# If "app", use app functions
+# If "py", use in-memory Python functions
+SWIFT_IMPL="py"
+
+SCRIPTS_TITAN=""
+if [[ ${SITE} == "titan" ]]
+then
+  SCRIPTS_TITAN=(
+    -script_file=$EMEWS_PROJECT_ROOT/scripts/titan_run_model.sh
+    -log_script=$WORKFLOWS_ROOT/common/sh/titan_run_logger.sh
+  )
+  # TODO:
+  SWIFT_IMPL="app"
+fi
+
+CMD_LINE_ARGS=( $* # Extra user args
+                -exp_id=$EXPID
                 -pp=$PROPOSE_POINTS
                 -mi=$MAX_ITERATIONS
                 -mb=$MAX_BUDGET
                 -ds=$DESIGN_SIZE
                 -param_set_file=$PARAM_SET_FILE
-                -script_file=$EMEWS_PROJECT_ROOT/scripts/titan_run_model.sh
-                -exp_id=$EXPID
-                -log_script=$WORKFLOWS_ROOT/common/sh/titan_run_logger.sh
+                ${SCRIPTS_TITAN[@]}
               )
 
 # Add any script variables that you want to log as
@@ -77,20 +95,22 @@ USER_VARS=($CMD_LINE_ARGS)
 # log variables and script to to TURBINE_OUTPUT directory
 log_script $EMEWS_PROJECT_ROOT/swift/$script_name
 
-export PROJECT=CSC249ADOA01
-
 set -x
-WORKFLOW_SWIFT=ai_workflow3.swift
-swift-t -m cray -n $PROCS\
-       -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
-       -p -I $EQR -r $EQR \
-       -e TURBINE_RESIDENT_WORK_WORKERS=$TURBINE_RESIDENT_WORK_WORKERS \
-       -e RESIDENT_WORK_RANKS=$RESIDENT_WORK_RANKS \
-       -e EMEWS_PROJECT_ROOT=$EMEWS_PROJECT_ROOT \
-       -e PYTHONPATH=$PYTHONPATH \
-       -e PYTHONHOME=$PYTHONHOME \
-       -e TURBINE_LOG=$TURBINE_LOG \
-       -e TURBINE_DEBUG=$TURBINE_DEBUG\
-       -e ADLB_DEBUG=$ADLB_DEBUG \
-       -e TURBINE_OUTPUT=$TURBINE_OUTPUT \
-       $EMEWS_PROJECT_ROOT/swift/$WORKFLOW_SWIFT ${CMD_LINE_ARGS[@]}
+WORKFLOW_SWIFT=workflow.swift
+swift-t -p $MACHINE \
+        -I $EMEWS_PROJECT_ROOT/swift \
+        -i log_$SWIFT_IMPL \
+        -i obj_$SWIFT_IMPL \
+        -n $PROCS \
+        -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+        -I $EQR -r $EQR \
+        -e TURBINE_RESIDENT_WORK_WORKERS=$TURBINE_RESIDENT_WORK_WORKERS \
+        -e RESIDENT_WORK_RANKS=$RESIDENT_WORK_RANKS \
+        -e EMEWS_PROJECT_ROOT=$EMEWS_PROJECT_ROOT \
+        -e PYTHONPATH=$PYTHONPATH \
+        -e PYTHONHOME=$PYTHONHOME \
+        -e TURBINE_LOG=$TURBINE_LOG \
+        -e TURBINE_DEBUG=$TURBINE_DEBUG\
+        -e ADLB_DEBUG=$ADLB_DEBUG \
+        -e TURBINE_OUTPUT=$TURBINE_OUTPUT \
+        $EMEWS_PROJECT_ROOT/swift/$WORKFLOW_SWIFT ${CMD_LINE_ARGS[@]}
