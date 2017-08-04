fbee65325cdd2205e20ea57296d9a5b7ae4fefdb
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-08-04 09:36:01 -0700

    Better defaults for testing

--- workflows/p3b1_mlrMBO/test/cfg-sys-1.sh
+++ workflows/p3b1_mlrMBO/test/cfg-sys-1.sh
@@ -6,10 +6,10 @@
 # The number of MPI processes
 # Note that 2 processes are reserved for Swift/EMEMS
 # The default of 4 gives you 2 workers, i.e., 2 concurrent Keras runs
-export PROCS=${PROCS:-258}
+export PROCS=${PROCS:-4}
 
 # MPI processes per node
 # Cori has 32 cores per node, 128GB per node
-export PPN=${PPN:-1}
-export QUEUE=${QUEUE:-batch}
+export PPN=${PPN:-4}
+export QUEUE=${QUEUE:-debug}
 export WALLTIME=${WALLTIME:-00:03:00}
