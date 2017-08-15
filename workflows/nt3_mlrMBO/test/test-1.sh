7d0e255f3f866d8d51dd57722c7171da08a6eda3
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-08-15 12:28:44 -0500

    Working small test for NT3/Theta

---
+++ workflows/nt3_mlrMBO/test/test-1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -eu
+
+# NT3 TEST 1
+
+if (( ${#} != 1 ))
+then
+  echo "usage: test SITE"
+  exit 1
+fi
+
+SITE=$1
+
+# Self-configure
+THIS=$( cd $( dirname $0 ) && /bin/pwd )
+EMEWS_PROJECT_ROOT=$( cd $THIS/.. && /bin/pwd )
+export EMEWS_PROJECT_ROOT
+WORKFLOWS_ROOT=$( cd $EMEWS_PROJECT_ROOT/.. && /bin/pwd )
+source $WORKFLOWS_ROOT/common/sh/utils.sh
+
+# Select configurations
+CFG_SYS=$THIS/cfg-sys-1.sh
+CFG_PRM=$THIS/cfg-prm-1.sh
+
+# Submit job
+$EMEWS_PROJECT_ROOT/swift/theta_workflow.sh $SITE -a $CFG_SYS $CFG_PRM
+
+# Wait for job
+TURBINE_OUTPUT=$( cat turbine-directory.txt )
+JOBID=$( cat $TURBINE_OUTPUT/jobid.txt )
+# queue_wait $SITE $JOBID
+
+# Check job output
+OUTPUT=$TURBINE_OUTPUT/output.txt
+WORKFLOW=$( basename $EMEWS_PROJECT_ROOT )
+SCRIPT=$( basename $0 .sh )
+check_output "learning_rate" $OUTPUT $WORKFLOW $SCRIPT $JOBID
+
+echo "$SCRIPT: SUCCESS"
