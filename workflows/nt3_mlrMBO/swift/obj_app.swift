9aac90380ed91d1a0571be8efd0f64378b5108d9
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-08-16 16:21:00 -0500

    Less chain operator

--- workflows/nt3_mlrMBO/swift/obj_app.swift
+++ workflows/nt3_mlrMBO/swift/obj_app.swift
@@ -2,11 +2,12 @@
 // OBJ APP
 
 (string obj_result) obj(string params, string iter_indiv_id) {
-  string outdir = "%s/run_%s" % (turbine_output, iter_indiv_id) =>
+  string outdir = "%s/run_%s" % (turbine_output, iter_indiv_id);
+  printf("outdir: %s", outdir);
+  string result_file = outdir/"result.txt";
   run_model(model_script, params, outdir, iter_indiv_id) =>
-  string result_file = "%s/result.txt" % outdir =>
-  obj_result = get_results(result_file);
-  printf(obj_result);
+    obj_result = get_results(result_file);
+  printf("result(%s): %s", params, obj_result);
 }
 
 app (void o) run_model (file shfile, string params_string, string instance, string run_id)
