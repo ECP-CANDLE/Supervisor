842a6673d913bda410c5c80287603931c04273bf
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-08-18 16:15:47 -0500

    Better output

--- workflows/nt3_mlrMBO/swift/obj_app.swift
+++ workflows/nt3_mlrMBO/swift/obj_app.swift
@@ -3,11 +3,11 @@
 
 (string obj_result) obj(string params, string iter_indiv_id) {
   string outdir = "%s/run_%s" % (turbine_output, iter_indiv_id);
-  printf("outdir: %s", outdir);
+  printf("run model: %s", outdir);
   string result_file = outdir/"result.txt";
   run_model(model_script, params, outdir, iter_indiv_id) =>
     obj_result = get_results(result_file);
-  printf("result(%s): %s", params, obj_result);
+  printf("result(%s): %s", iter_indiv_id, obj_result);
 }
 
 app (void o) run_model (file shfile, string params_string, string instance, string run_id)
