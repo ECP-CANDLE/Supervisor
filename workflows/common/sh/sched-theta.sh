e62481b6bcb67805e52f04be1f2c3b81aa8440e5
Rajeev Jain <rajeeja@gmail.com>
2017-08-18 21:32:54 +0000

    Add PROJECT and QUEUE in sched

--- workflows/common/sh/sched-theta.sh
+++ workflows/common/sh/sched-theta.sh
@@ -3,5 +3,7 @@
 # Scheduler settings for Swift/Theta
 
 # Tell Swift/T to generate and submit a Theta-formatted script
+export QUEUE=${QUEUE:-debug-flat-quad}
+export PROJECT=Candle_ECP
 MACHINE="-m theta"
 
