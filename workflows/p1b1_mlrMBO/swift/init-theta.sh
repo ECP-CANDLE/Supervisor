7cf4312c6f28ddd55d5fe284788058988cd4174a
Rajeev Jain <rajeeja@gmail.com>
2017-08-20 02:05:01 +0000

    A step closer to getting runs on theta

--- workflows/p1b1_mlrMBO/swift/init-theta.sh
+++ workflows/p1b1_mlrMBO/swift/init-theta.sh
@@ -3,5 +3,5 @@ set -eu
 
 source $EMEWS_PROJECT_ROOT/etc/emews_utils.sh
 
-log_script $EMEWS_PROJECT_ROOT/swift/workflow.sh
+log_script $EMEWS_PROJECT_ROOT/swift/theta_workflow.sh
 
--- workflows/p1b1_mlrMBO/swift/theta_workflow.sh
+++ workflows/p1b1_mlrMBO/swift/theta_workflow.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 set -eu
 
-# THETA WORKFLOW
-# Main entry point for P1B1 mlrMBO workflow
-
 # Autodetect this workflow directory
 export EMEWS_PROJECT_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
 
@@ -13,51 +10,72 @@ export EMEWS_PROJECT_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
 
 # The directory in the Benchmarks repo containing P1B3
 BENCHMARK_DIR=$EMEWS_PROJECT_ROOT/../../../Benchmarks/Pilot1/P1B1
+
 # The number of MPI processes
 # Note that 2 processes are reserved for Swift/EMEMS
 # The default of 4 gives you 2 workers, i.e., 2 concurrent Keras runs
-export PROCS=${PROCS:-10}
+export PROCS=${PROCS:-32}
 
 # MPI processes per node
-# (Theta has 64 cores per node, 192GB per node)
+# Cori has 32 cores per node, 128GB per node
 export PPN=${PPN:-1}
 
 export QUEUE=${QUEUE:-default}
-export WALLTIME=${WALLTIME:-02:00:00}
+export WALLTIME=${WALLTIME:-00:40:00}
 
 # Benchmark run timeout: benchmark run will timeouT
 # after the specified number of seconds. -1 is no timeout.
-BENCHMARK_TIMEOUT=${BENCHMARK_TIMEOUT:-1800}
+BENCHMARK_TIMEOUT=${BENCHMARK_TIMEOUT:-3600}
+
 
 # mlrMBO settings
-# How many to runs evaluate per iteration
-
-#MAX_BUDGET=${MAX_BUDGET:-110}
+#MAX_BUDGET=${MAX_BUDGET:-1000}
 # Total iterations
-MAX_ITERATIONS=${MAX_ITERATIONS:-4}
-#DESIGN_SIZE=${DESIGN_SIZE:-
-PROPOSE_POINTS=${PROPOSE_POINTS:-8}
+MAX_ITERATIONS=${MAX_ITERATIONS:-3}
+#DESIGN_SIZE=${DESIGN_SIZE:-300}
+PROPOSE_POINTS=${PROPOSE_POINTS:-300}
 PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/temp_parameter_set.R}
-
-# pbalabra:
-# PARAM_SET_FILE="$EMEWS_PROJECT_ROOT/data/parameter_set1.R"
+MODEL_NAME="p1b1"
 
 # USER SETTINGS END
+
 
 # Source some utility functions used by EMEWS in this script
 source "${EMEWS_PROJECT_ROOT}/etc/emews_utils.sh"
 
-WORKFLOWS_ROOT=$( cd $EMEWS_PROJECT_ROOT ; cd .. ; /bin/pwd )
-source $WORKFLOWS_ROOT/common/sh/langs-theta.sh
-source $WORKFLOWS_ROOT/common/sh/utils.sh
+if [ "$#" -ne 1 ]; then
+  script_name=$(basename $0)
+  echo "Usage: ${script_name} EXPERIMENT_ID (e.g. ${script_name} experiment_1)"
+  exit 1
+fi
 
 # uncomment to turn on swift/t logging. Can also set TURBINE_LOG,
 # TURBINE_DEBUG, and ADLB_DEBUG to 0 to turn off logging
-# export TURBINE_LOG=1 # TURBINE_DEBUG=1 ADLB_DEBUG=1
+export TURBINE_LOG=1 TURBINE_DEBUG=1 ADLB_DEBUG=1
 
-get_expid $*
+export EXPID=$1
+export TURBINE_OUTPUT_ROOT=${TURBINE_OUTPUT_ROOT:-$EMEWS_PROJECT_ROOT/experiments}
+export TURBINE_OUTPUT=$TURBINE_OUTPUT_ROOT/$EXPID
+check_directory_exists
 
 export TURBINE_JOBNAME="${EXPID}_job"
+
+# if R cannot be found, then these will need to be
+# uncommented and set correctly.
+# export R_HOME=/path/to/R
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
+# export PYTHONHOME=
+
+TCL=/home/wozniak/Public/sfw/theta/tcl-8.6.1
+export R=/home/wozniak/Public/sfw/theta/R-3.4.0/lib64/R
+export PY=/home/wozniak/Public/sfw/theta/Python-2.7.12
+export LD_LIBRARY_PATH=$PY/lib:$R/lib:$LD_LIBRARY_PATH
+COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
+PYTHONPATH=$EMEWS_PROJECT_ROOT/python:$BENCHMARK_DIR:$COMMON_DIR:$BENCHMARK_DIR/../../common
+PYTHONHOME=/home/wozniak/Public/sfw/theta/Python-2.7.12
+
+export PATH=/home/wozniak/Public/sfw/theta/swift-t-pyr/stc/bin:$TCL/bin:$PATH
+#$PYTHONHOME/bin:$TCL/bin:$PATH
 
 # Resident task workers and ranks
 export TURBINE_RESIDENT_WORK_WORKERS=1
@@ -66,40 +84,43 @@ export RESIDENT_WORK_RANKS=$(( PROCS - 2 ))
 # EQ/R location
 EQR=$EMEWS_PROJECT_ROOT/ext/EQ-R
 
-SCRIPT_FILE=$EMEWS_PROJECT_ROOT/scripts/theta_run_model.sh
-LOG_SCRIPT=$EMEWS_PROJECT_ROOT/../common/sh/theta_run_logger.sh
-
-CMD_LINE_ARGS=( $*
-                -exp_id=$EXPID
-                -pp=$PROPOSE_POINTS
-                -it=$MAX_ITERATIONS
-                -param_set_file=$PARAM_SET_FILE
-                -script_file=$SCRIPT_FILE
-                -log_script=$LOG_SCRIPT
-                -benchmark_timeout=$BENCHMARK_TIMEOUT
-
-              )
+CMD_LINE_ARGS="$* -pp=$PROPOSE_POINTS -it=$MAX_ITERATIONS "
+CMD_LINE_ARGS+="-param_set_file=$PARAM_SET_FILE -script_file=$EMEWS_PROJECT_ROOT/scripts/theta_run_model.sh "
+CMD_LINE_ARGS+="-model_name=$MODEL_NAME "
+CMD_LINE_ARGS+="-exp_id=$EXPID -log_script=$EMEWS_PROJECT_ROOT/../common/sh/theta_run_logger.sh "
+CMD_LINE_ARGS+="-benchmark_timeout=$BENCHMARK_TIMEOUT"
 
 TURBINE_DIR=/home/wozniak/Public/sfw/theta/swift-t-pyr/turbine/lib
-export PROJECT=Candle_ECP
+
+# set machine to your scheduler type (e.g. pbs, slurm, cobalt etc.),
+# or empty for an immediate non-queued unscheduled run
+MACHINE="theta"
+
+if [ -n "$MACHINE" ]; then
+  MACHINE="-m $MACHINE"
+fi
+
 # Add any script variables that you want to log as
 # part of the experiment meta data to the USER_VARS array,
 # for example, USER_VARS=("VAR_1" "VAR_2")
 USER_VARS=($CMD_LINE_ARGS)
 # log variables and script to to TURBINE_OUTPUT directory
-# log_script
+log_script
 
+export PROJECT=MOAB_App
 # echo's anything following this to standard out
-set -x
-WORKFLOW_SWIFT=$EMEWS_PROJECT_ROOT/swift/workflow.swift
-swift-t -m theta \
-        -n $PROCS \
-        -p -I $EQR -r $EQR -r $TURBINE_DIR \
-        -t i:$EMEWS_PROJECT_ROOT/swift/init-theta.sh \
+set -xi
+echo $PYTHONPATH
+WORKFLOW_SWIFT=ai_workflow3.swift
+swift-t -n $PROCS $MACHINE -p -I $EQR -r $EQR  -r $TURBINE_DIR \
         -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
-        -e TURBINE_RESIDENT_WORK_WORKERS \
-        -e RESIDENT_WORK_RANKS \
-        -e EMEWS_PROJECT_ROOT \
-        -e PYTHONPATH=$PYTHONPATH \
-        -e PYTHONHOME=$PYTHONHOME \
-        $WORKFLOW_SWIFT ${CMD_LINE_ARGS[@]}
+        -e TURBINE_RESIDENT_WORK_WORKERS=$TURBINE_RESIDENT_WORK_WORKERS \
+    -e RESIDENT_WORK_RANKS=$RESIDENT_WORK_RANKS \
+    -e EMEWS_PROJECT_ROOT=$EMEWS_PROJECT_ROOT \
+    -e PYTHONPATH=$PYTHONPATH \
+    -e PYTHONHOME=$PYTHONHOME \
+    -e TURBINE_LOG=$TURBINE_LOG \
+    -e TURBINE_DEBUG=$TURBINE_DEBUG\
+    -e ADLB_DEBUG=$ADLB_DEBUG \
+    -e TURBINE_OUTPUT=$TURBINE_OUTPUT \
+        $EMEWS_PROJECT_ROOT/swift/$WORKFLOW_SWIFT $CMD_LINE_ARGS
