8b57cd5ff7a5d674c9503d355fabad1b6e78474f
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-10-03 10:44:21 -0500

    New SITE=default

---
+++ workflows/common/sh/langs-default.sh
@@ -0,0 +1,12 @@
+
+# LANGS DEFAULT
+
+# Assumes everything is in your paths, except the project Python code
+
+export PYTHONPATH=${EMEWS_PROJECT_ROOT}:${PYTHONPATH:-}
+
+SWIFT_IMPL=app
+
+# Resident task workers and ranks
+export TURBINE_RESIDENT_WORK_WORKERS=1
+export RESIDENT_WORK_RANKS=$(( PROCS - 2 ))
