e084ecf387fd45b551ee5e61f33f834051583ab2
Justin Wozniak <wozniak@mcs.anl.gov>
2017-10-04 16:59:52 -0500

    Use python_envs

--- workflows/p1b1_mlrMBO/swift/workflow.sh
+++ workflows/p1b1_mlrMBO/swift/workflow.sh
@@ -70,7 +70,6 @@ USER_VARS=( $CMD_LINE_ARGS )
 # log variables and script to to TURBINE_OUTPUT directory
 log_script
 
-# echo's anything following this to standard out
 WORKFLOW_SWIFT=workflow.swift
 swift-t -n $PROCS \
         $MACHINE  \
@@ -82,11 +81,9 @@ swift-t -n $PROCS \
         -e TURBINE_RESIDENT_WORK_WORKERS=$TURBINE_RESIDENT_WORK_WORKERS \
         -e RESIDENT_WORK_RANKS=$RESIDENT_WORK_RANKS \
         -e EMEWS_PROJECT_ROOT=$EMEWS_PROJECT_ROOT \
-        -e PYTHONPATH=$PYTHONPATH \
-        -e PYTHONHOME=$PYTHONHOME \
+        $( python_envs ) \
         -e TURBINE_LOG=$TURBINE_LOG \
         -e TURBINE_DEBUG=$TURBINE_DEBUG\
         -e ADLB_DEBUG=$ADLB_DEBUG \
         -e TURBINE_OUTPUT=$TURBINE_OUTPUT \
         $EMEWS_PROJECT_ROOT/swift/$WORKFLOW_SWIFT ${CMD_LINE_ARGS[@]}
-
