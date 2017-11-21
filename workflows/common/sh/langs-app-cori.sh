6e6e571ba2b3a2ad22a2bb74766cf998f8c7b74e
Nick Collier <nick.collier@gmail.com>
2017-11-21 17:09:22 -0500

    Fixed order of env var setting

--- workflows/common/sh/langs-app-cori.sh
+++ workflows/common/sh/langs-app-cori.sh
@@ -1,15 +1,7 @@
 # Uncomment this if want to run with tensorflow loaded from:
 # module load tensorflow/intel-head. See 
 # http://www.nersc.gov/users/data-analytics/data-analytics-2/deep-learning/using-tensorflow-at-nersc/
-
-# Cori / Tensorflow env vars
-# export KMP_BLOCKTIME=30
-# export KMP_SETTINGS=1
-# export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
-# export OMP_NUM_THREADS=136
-# export NUM_INTER_THREADS=1
-# export NUM_INTRA_THREADS=136
-# 
+ 
 # module swap craype-haswell craype-mic-knl
 # module load tensorflow/intel-head 
 # # module load tensorflow/intel-head sets these:
@@ -19,6 +11,15 @@
 # #setenv		 NUM_INTER_THREADS 2 
 # #setenv		 NUM_INTRA_THREADS 16 
 # #setenv		 OMP_NUM_THREADS 16 
+
+# Cori / Tensorflow env vars
+# export KMP_BLOCKTIME=30
+# export KMP_SETTINGS=1
+# export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
+# export OMP_NUM_THREADS=136
+# export NUM_INTER_THREADS=1
+# export NUM_INTRA_THREADS=136
+
 # 
 # export PYTHONHOME="/usr/common/software/tensorflow/intel-tensorflow/head"
 # PYTHON="$PYTHONHOME/bin/python"
