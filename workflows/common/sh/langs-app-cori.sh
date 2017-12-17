d723a485529163eed2f684ab39c27e6d0c132b54
brettin <brettin@anl.gov>
2017-12-17 14:26:50 -0800

    added cori settings

--- workflows/common/sh/langs-app-cori.sh
+++ workflows/common/sh/langs-app-cori.sh
@@ -2,8 +2,8 @@
 # module load tensorflow/intel-head. See 
 # http://www.nersc.gov/users/data-analytics/data-analytics-2/deep-learning/using-tensorflow-at-nersc/
  
-# module swap craype-haswell craype-mic-knl
-# module load tensorflow/intel-head 
+module swap craype-haswell craype-mic-knl
+module load tensorflow/intel-head 
 # # module load tensorflow/intel-head sets these:
 # #setenv		 KMP_BLOCKTIME 1 
 # #setenv		 KMP_SETTINGS 1 
@@ -13,21 +13,21 @@
 # #setenv		 OMP_NUM_THREADS 16 
 
 # Cori / Tensorflow env vars
-# export KMP_BLOCKTIME=30
-# export KMP_SETTINGS=1
-# export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
-# export OMP_NUM_THREADS=136
-# export NUM_INTER_THREADS=1
-# export NUM_INTRA_THREADS=136
+export KMP_BLOCKTIME=1
+export KMP_SETTINGS=1
+export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
+export OMP_NUM_THREADS=68
+export NUM_INTER_THREADS=1
+export NUM_INTRA_THREADS=68
 
-# 
-# export PYTHONHOME="/usr/common/software/tensorflow/intel-tensorflow/head"
-# PYTHON="$PYTHONHOME/bin/python"
-# export LD_LIBRARY_PATH="$PYTHONHOME/lib"
-# export PATH="$PYTHONHOME/bin:$PATH"
-# 
-# COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
-# PYTHONPATH+=":$PYTHONHOME/lib/python2.7:"
-# PYTHONPATH+=":$COMMON_DIR:"
-# PYTHONPATH+="$PYTHONHOME/lib/python2.7/site-packages"
-# export PYTHONPATH
+ 
+export PYTHONHOME="/usr/common/software/tensorflow/intel-tensorflow/head"
+PYTHON="$PYTHONHOME/bin/python"
+export LD_LIBRARY_PATH="$PYTHONHOME/lib"
+export PATH="$PYTHONHOME/bin:$PATH"
+ 
+COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
+PYTHONPATH+=":$PYTHONHOME/lib/python2.7:"
+PYTHONPATH+=":$COMMON_DIR:"
+PYTHONPATH+="$PYTHONHOME/lib/python2.7/site-packages"
+export PYTHONPATH
