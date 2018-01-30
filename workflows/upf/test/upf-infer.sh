5f932a07a3aece7983c140a81e57d8694d649b93
Justin Wozniak <wozniak@mcs.anl.gov>
2018-01-30 11:44:46 -0600

    Sketch of how to run infer workflow

---
+++ workflows/upf/test/upf-infer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+# TEST UPF 1
+
+if (( ${#} != 1 ))
+then
+  echo "usage: test SITE"
+  exit 1
+fi
+
+SITE=$1
+
+# Self-configure
+THIS=$(               cd $( dirname $0 ) ; /bin/pwd )
+EMEWS_PROJECT_ROOT=$( cd $THIS/..        ; /bin/pwd )
+WORKFLOWS_ROOT=$(     cd $THIS/../..     ; /bin/pwd )
+export EMEWS_PROJECT_ROOT
+
+export MODEL_NAME="p1b1"
+CFG_SYS=$THIS/cfg-sys-1.sh
+
+$EMEWS_PROJECT_ROOT/swift/workflow.sh $SITE -a $CFG_SYS $THIS/upf-infer.txt
