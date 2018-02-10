e08c68288ab52dd7b712763584d9908634bccbb5
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-02-10 09:42:36 -0800

    Update threads on Cori to 16 for infer workflow

--- workflows/common/sh/langs-app-cori.sh
+++ workflows/common/sh/langs-app-cori.sh
@@ -3,18 +3,18 @@
 # Language settings for Cori app functions (Python, R, etc.)
 
 # Uncomment this if want to run with tensorflow loaded from:
-# module load tensorflow/intel-head. See 
+# module load tensorflow/intel-head. See
 # http://www.nersc.gov/users/data-analytics/data-analytics-2/deep-learning/using-tensorflow-at-nersc/
- 
+
 module swap craype-haswell craype-mic-knl
-module load tensorflow/intel-head 
+module load tensorflow/intel-head
 # # module load tensorflow/intel-head sets these:
-# #setenv		 KMP_BLOCKTIME 1 
-# #setenv		 KMP_SETTINGS 1 
-# #setenv		 KMP_AFFINITY granularity=fine,verbose,compact,1,0 
-# #setenv		 NUM_INTER_THREADS 2 
-# #setenv		 NUM_INTRA_THREADS 16 
-# #setenv		 OMP_NUM_THREADS 16 
+# #setenv		 KMP_BLOCKTIME 1
+# #setenv		 KMP_SETTINGS 1
+# #setenv		 KMP_AFFINITY granularity=fine,verbose,compact,1,0
+# #setenv		 NUM_INTER_THREADS 2
+# #setenv		 NUM_INTRA_THREADS 16
+# #setenv		 OMP_NUM_THREADS 16
 
 # Cori / Tensorflow env vars
 # These values are as recommended in
@@ -22,15 +22,15 @@ module load tensorflow/intel-head
 export KMP_BLOCKTIME=1
 export KMP_SETTINGS=1
 export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
-export OMP_NUM_THREADS=68
+export OMP_NUM_THREADS=16
 export NUM_INTER_THREADS=1
-export NUM_INTRA_THREADS=68
- 
+export NUM_INTRA_THREADS=16
+
 export PYTHONHOME="/usr/common/software/tensorflow/intel-tensorflow/head"
 PYTHON="$PYTHONHOME/bin/python"
 export LD_LIBRARY_PATH="$PYTHONHOME/lib"
 export PATH="$PYTHONHOME/bin:$PATH"
- 
+
 COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
 PYTHONPATH+=":$PYTHONHOME/lib/python2.7:"
 PYTHONPATH+=":$COMMON_DIR:"
