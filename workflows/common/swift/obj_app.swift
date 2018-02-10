17fd1b5793ce7fedc6c610b95f5b3a9593ed5afd
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-02-10 09:43:56 -0800

    Rename iter_indiv_id to run_id

--- workflows/common/swift/obj_app.swift
+++ workflows/common/swift/obj_app.swift
@@ -9,25 +9,28 @@
 //   printf(obj_result);
 // }
 
-(string obj_result) obj(string params, string iter_indiv_id, string obj_param) {
+(string obj_result) obj(string params,
+                        string run_id,
+                        string obj_param) {
   string model_sh       = getenv("MODEL_SH");
   string turbine_output = getenv("TURBINE_OUTPUT");
 
-  string outdir = "%s/run/%s" % (turbine_output, iter_indiv_id);
+  string outdir = "%s/run/%s" % (turbine_output, run_id);
   // printf("running model shell script in: %s", outdir);
+  // We do not use a file type here because this file may not be created:
   string result_file = outdir/"result.txt";
-  wait (run_model(model_sh, params, iter_indiv_id, obj_param))
+  wait (run_model(model_sh, params, run_id, obj_param))
   {
     obj_result = get_results(result_file);
   }
-  printf("result(%s): %s", iter_indiv_id, obj_result);
+  printf("result(%s): %s", run_id, obj_result);
 }
 
 app (void o) run_model (string model_sh, string params,
                         string runid, string obj_param)
 {
-  //                      1       2       3        4 
-      "bash" model_sh FRAMEWORK params   runid  obj_param;
+  //                  1       2       3        4
+  "bash" model_sh FRAMEWORK params   runid  obj_param;
 }
 
 (string obj_result) get_results(string result_file) {
