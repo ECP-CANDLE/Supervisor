5888ee3891c8d712b179f324751cb569dae0c018
Justin Wozniak <wozniak@mcs.anl.gov>
2018-03-07 17:35:15 -0600

    Fix test

--- workflows/upf/test/test-json.swift
+++ workflows/upf/test/test-json.swift
@@ -2,10 +2,8 @@
 import json;
 
 // Sample UPF fragment:
-s = """
-{ 'model_file':   '/project/projectdirs/m2924/brettin/combo-inference/uq.0/save/combo.A=relu.B=32.E=50.O=adam.LR=None.CF=r.DF=d.wu_lr.re_lr.res.D1=1000.D2=1000.D3=1000.D4=1000.model.h5', 'weights_file': '/project/projectdirs/m2924/brettin/combo-inference/uq.0/save/combo.A=relu.B=32.E=50.O=adam.LR=None.CF=r.DF=d.wu_lr.re_lr.res.D1=1000.D2=1000.D3=1000.D4=1000.weights.h5', 'drug_set':     'ALMANAC', 'sample_set':   'GDSC' }
-""";
+s = "{ \"model_file\":   \"/project/projectdirs/m2924/brettin/combo-inference/uq.0/save/combo.A=relu.B=32.E=50.O=adam.LR=None.CF=r.DF=d.wu_lr.re_lr.res.D1=1000.D2=1000.D3=1000.D4=1000.model.h5\", \"weights_file\": \"/project/projectdirs/m2924/brettin/combo-inference/uq.0/save/combo.A=relu.B=32.E=50.O=adam.LR=None.CF=r.DF=d.wu_lr.re_lr.res.D1=1000.D2=1000.D3=1000.D4=1000.weights.h5\", \"drug_set\":     \"ALMANAC\", \"sample_set\":   \"GDSC\" } ";
 
-trace(s);
+// Extract one entry:
 trace("model_file: ", json_get(s, "sample_set"));
 
