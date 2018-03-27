75f24ad5539ead90a891ba517d5245aacd854b96
Nick Collier <ncollier@anl.gov>
2018-03-27 11:56:40 -0400

    Added script for local running

---
+++ workflows/pbt/scripts/local_run_pbt.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+set -eu
+
+PROCS=$1
+EXP_ID=$2
+MODEL_NAME=$3
+
+THIS=$( cd $( dirname $0 ) ; /bin/pwd )
+ROOT="$THIS/.."
+EXP_DIR="$ROOT/experiments/$EXP_ID"
+PBT_PY="$ROOT/python/pbt.py"
+
+BENCHMARKS=$HOME/Documents/repos/Benchmarks
+SUPERVISOR=$( cd "$PWD/../../.."  ; /bin/pwd )
+
+PYTHONPATH=$BENCHMARKS/Pilot1/common
+PYTHONPATH+=":$BENCHMARKS/common"
+PYTHONPATH+=":$SUPERVISOR/workflows/common/python"
+PYTHONPATH+=":$ROOT/models/TC1"
+
+export PYTHONPATH=$PYTHONPATH
+
+mkdir -p $EXP_DIR
+
+PARAMS_FILE=$4
+cp $ROOT/data/$PARAMS_FILE $EXP_DIR/
+cd $EXP_DIR
+
+mpirun -n $PROCS python $PBT_PY $PARAMS_FILE $EXP_DIR tc1 $EXP_ID
+
+cd $THIS
