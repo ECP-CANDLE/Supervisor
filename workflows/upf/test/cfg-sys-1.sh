37a45084a60a6600aacc9e1019c4100e88a24ac6
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-04-18 08:12:21 -0700

    Restore UPF cfg-sys-1

--- workflows/upf/test/cfg-sys-1.sh
+++ workflows/upf/test/cfg-sys-1.sh
@@ -5,7 +5,7 @@
 # Note that 1 processes is reserved for Swift/T
 # For example, if PROCS=4 that gives you 3 workers,
 # i.e., 3 concurrent Keras runs.
-export PROCS=${PROCS:-2}
+export PROCS=${PROCS:-4}
 
 # MPI processes per node.  This should not exceed PROCS.
 # Cori has 32 cores per node, 128GB per node
@@ -15,17 +15,22 @@ export PPN=${PPN:-1}
 
 # Cori: (cf. sched-cori)
 # export QUEUE=${QUEUE:-debug}
-# export QUEUE=debug
+# Cori queues: debug, regular
+# export QUEUE=regular
+export QUEUE=debug
 # CANDLE on Cori:
 # export PROJECT=m2924
 
 # Theta: (cf. sched-theta)
-export QUEUE=${QUEUE:-debug-cache-quad}
-export PROJECT=${PROJECT:-ecp-testbed-01}
-#export PROJECT=Candle_ECP
-#export PROJECT=CSC249ADOA01
+# export QUEUE=${QUEUE:-debug-cache-quad}
+# export PROJECT=${PROJECT:-ecp-testbed-01}
+# export PROJECT=Candle_ECP
+# export PROJECT=CSC249ADOA01
 
 export WALLTIME=${WALLTIME:-00:10:00}
+
+# export MAIL_ENABLED=1
+# export MAIL_ADDRESS=wozniak@mcs.anl.gov
 
 # Benchmark run timeout: benchmark run will timeouT
 # after the specified number of seconds. -1 is no timeout.
