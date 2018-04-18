7189f924ae9dd77f0668dafab8ff87eb236d451d
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-04-18 12:58:04 -0700

    Build settings for Cori

--- workflows/test-horovod/build.sh
+++ workflows/test-horovod/build.sh
@@ -3,7 +3,15 @@ set -e
 
 # HOROVOD BUILD SH
 
-CONTROLLER=$HOME/proj/horovod/controller
+# Cori:
+PATH=/global/homes/w/wozniak/Public/sfw/mpich-3.2.1/bin:$PATH
+PATH=/global/homes/w/wozniak/Public/sfw/tcl-8.6.6/bin:$PATH
+PATH=$HOME/Public/sfw/login/swift-t-2018-04-16/turbine/bin:$PATH
+
+# USER: set the Horovod location here:
+HOROVOD=$HOME/proj/horovod
+
+CONTROLLER=$HOROVOD/controller
 
 source $( turbine -C )
 
@@ -16,7 +24,5 @@ swig -I$MPI/include -I$CONTROLLER horovod.i
 
 mpicc -c -fPIC $TCL_INCLUDE_SPEC -I$CONTROLLER horovod_wrap.c
 mpicc -shared -o libhorovod.so horovod_wrap.o $CONTROLLER/controller.o \
-      -l python2.7 
+      -l python2.7
 tclsh make-package.tcl > pkgIndex.tcl
-
-stc -r $PWD test-horovod.swift
