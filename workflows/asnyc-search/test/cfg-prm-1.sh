97f20319d6750e8ac295eea63b32fe67b9cf65e8
jozik <jozik@anl.gov>
2018-04-20 15:16:40 -0500

    initial working version of async-search

---
+++ workflows/asnyc-search/test/cfg-prm-1.sh
@@ -0,0 +1,9 @@
+# CFG PRM test-1
+
+# async-search settings
+
+# Note: INIT_SIZE needs to be larger than PROCS-2 for now.
+INIT_SIZE=${INIT_SIZE:-4}
+MAX_EVALS=${MAX_EVALS:-8}
+NUM_BUFFER=${NUM_BUFFER:-2}
+RANDOM_SEED=${RANDOM_SEED:-0}
