363f930e8385854a506089da9cb514c8a9ca1627
Rajeev Jain <rajeeja@gmail.com>
2018-04-26 10:34:42 -0500

    o Deleting Benchmark_mlrMBO o workflows/mlrMBO is capable of running all benchmarks

--- workflows/p2b1_mlrMBO/data/parameter_set.R
+++
@@ -1,6 +0,0 @@
-# see https://cran.r-project.org/web/packages/ParamHelpers/ParamHelpers.pdfmakeNum
-# the parameter names should match names of the arguments expected by
-# the benchmark
-param.set <- makeParamSet(
-  makeIntegerParam("epochs", lower = 2, upper = 4)
-)
