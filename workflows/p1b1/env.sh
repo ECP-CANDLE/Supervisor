7ccb86b2c15c335d7683e008d43598ff4ed55565
Rajeev Jain <rajeeja@gmail.com>
2018-04-27 11:14:59 -0500

    o Delete p1b1 directory

--- workflows/p1b1/env.sh
+++
@@ -1,8 +0,0 @@
-
-# Pasted from paste.txt. This is easier to just source from the shell.  -Wozniak
-
-export LD_LIBRARY_PATH=/soft/libraries/unsupported/cudnn-7.5.1/cuda/lib64:$LD_LIBRARY_PATH
-export LIBRARY_PATH=/soft/libraries/unsupported/cudnn-7.5.1/cuda/lib64:$LIBRARY_PATH
-export CPATH=/soft/libraries/unsupported/cudnn-7.5.1/cuda/include:$CPATH
-source activate Candle_ML
-echo
