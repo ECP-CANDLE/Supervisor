c50cf8381c111a75d491f4c0243744182c75f813
Justin Wozniak <wozniak@mcs.anl.gov>
2018-04-27 12:21:53 -0500

    Initial async-local program

---
+++ workflows/async-local/utils.py
@@ -0,0 +1,35 @@
+
+import json
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+def depth(l):
+    if isinstance(l, list):
+        return 1 + max(depth(item) for item in l)
+    else:
+        return 0
+
+def create_list_of_json_strings(list_of_lists, params, super_delim=";"):
+    # create string of ; separated jsonified maps
+    res = []
+    if (depth(list_of_lists) == 1):
+        list_of_lists = [list_of_lists]
+
+    for l in list_of_lists:
+        jmap = {}
+        for i,p in enumerate(params):
+            jmap[p] = l[i]
+
+        jstring = json.dumps(jmap, cls=MyEncoder)
+        res.append(jstring)
+
+    return res, (super_delim.join(res))
