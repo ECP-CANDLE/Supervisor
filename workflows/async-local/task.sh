285ec529ac5515b9db3ee5a9c9e551eea08ea6a7
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-05-03 14:26:16 -0500

    Working version; add docs

--- workflows/async-local/task.sh
+++ workflows/async-local/task.sh
@@ -4,22 +4,23 @@ set -eu
 # TASK SH
 # Dummy task for testing
 
-if (( ${#} != 1 ))
+if (( ${#} != 2 ))
 then
-  echo "Requires PARAMS!"
+  echo "task.sh: requires PARALLELISM PARAMS"
   exit 1
 fi
 
 WORKFLOWS=$( cd $( dirname 0 )/.. ; /bin/pwd )
 source $WORKFLOWS/common/sh/utils.sh
 
-PARAMS=$1
-
-T=$(( $RANDOM % 3 ))
+PARALLELISM=$1
+PARAMS=$2
 
 echo "PID: $$"
-echo $PARAMS | print_json
-echo "delay $T"
+# echo $PARAMS | print_json
 echo
 
-sleep $T
+module load alps
+
+set -x
+aprun --pes $PARALLELISM bash -c 'sleep 1'
