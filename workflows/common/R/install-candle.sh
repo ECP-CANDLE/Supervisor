242e67806ae1c6822bb5e434c30af060ff497129
Justin Wozniak <wozniak@mcs.anl.gov>
2018-05-15 10:58:40 -0500

    Confirm option for non-interactive install (ref #43)

--- workflows/common/R/install-candle.sh
+++ workflows/common/R/install-candle.sh
@@ -5,16 +5,32 @@ set -eu
 
 # Installs all R packages needed for Supervisor workflows
 
+CONFIRM=1
+while getopts ":y" OPTION
+do
+  case $OPTION in
+    y) CONFIRM=0
+       ;;
+    *) # The shell error message was disabled above
+       echo "install-candle.sh: unknown option: $*"
+       exit 1
+       ;;
+  esac
+done
+
 echo "This will install multiple R packages for CANDLE."
 echo
 echo "using R:        $( which R )"
 echo "using gcc:      $( which gcc )"
 echo "using gfortran: $( which gfortran )"
 echo
-sleep 1
-echo "Press enter to confirm, or Ctrl-C to cancel."
 
-read _
+if [ $CONFIRM = 1 ]
+then
+  sleep 1
+  echo "Press enter to confirm, or Ctrl-C to cancel."
+  read _
+fi
 
 THIS=$( dirname $0 )
 nice R -f $THIS/install-candle.R
