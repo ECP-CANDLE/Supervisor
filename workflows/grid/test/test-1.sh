4026224e710e949c678a24702c19db54c1f00612
Rajeev Jain <rajeeja@gmail.com>
2018-05-31 17:22:10 -0500

    o Move to archives directory o Fixing grid for all Benchmarks

--- workflows/grid/test/test-1.sh
+++ workflows/grid/test/test-1.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -eu
 
-# P1B1 TEST 1
+# MLRMBO TEST 1
 
-if (( ${#} != 1 ))
+usage()
+{
+  echo "Usage: test BENCHMARK_NAME SITE RUN_DIR(optional)"
+  echo "       RUN_DIR is optional, use -a for automatic"
+}
+
+RUN_DIR=""
+if (( ${#} == 3 ))
 then
-  echo "usage: test SITE"
-  exit 1
+	RUN_DIR=$3
+elif (( ${#} == 2 )) # test-all uses this
+then
+	RUN_DIR="-a"
+else
+        usage
+        exit 1
 fi
 
-SITE=$1
+export MODEL_NAME=$1
+SITE=$2
 
 # Self-configure
 THIS=$( cd $( dirname $0 ) && /bin/pwd )
@@ -19,20 +32,37 @@ WORKFLOWS_ROOT=$( cd $EMEWS_PROJECT_ROOT/.. && /bin/pwd )
 source $WORKFLOWS_ROOT/common/sh/utils.sh
 
 # Select configurations
-CFG_SYS=$THIS/cfg-sys-1.sh
-CFG_PRM=$THIS/cfg-prm-1.sh
+export CFG_SYS=$THIS/cfg-sys-1.sh
+export CFG_PRM=$THIS/cfg-prm-1.sh
+
+# Specify the R file for This file must be present in the $EMEWS_PROJECT_ROOT/R
+export R_FILE=mlrMBO1.R
+
+# What to return from the objective function (Keras model)
+# val_loss (default) and val_corr are supported
+export OBJ_RETURN="val_loss"
+
+if [[ $SITE == "theta" ]]
+then
+  export WAIT=1
+fi
 
 # Submit job
-$EMEWS_PROJECT_ROOT/swift/workflow.sh $SITE -a $CFG_SYS $CFG_PRM
+$EMEWS_PROJECT_ROOT/swift/workflow.sh $SITE $RUN_DIR $CFG_SYS $CFG_PRM $MODEL_NAME
 
 # Wait for job
 queue_wait
 
+cp $0 $TURBINE_OUTPUT
 # Check job output
 OUTPUT=$TURBINE_OUTPUT/output.txt
 WORKFLOW=$( basename $EMEWS_PROJECT_ROOT )
+
 SCRIPT=$( basename $0 .sh )
 check_output "learning_rate" $OUTPUT $WORKFLOW $SCRIPT $JOBID
 
 echo "$SCRIPT: SUCCESS"
 
+# Local Variables:
+# c-basic-offset: 2;
+# End:
