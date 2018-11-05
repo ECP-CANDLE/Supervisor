5a0c25147ce4fa3664612235095923ad1c96db27
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-11-05 15:37:36 -0600

    Initial Fake-LBANN example

---
+++ scratch/fake-lbann/fl_interface.i
@@ -0,0 +1,8 @@
+
+%module fl_interface
+
+%include "fl_interface.h"
+
+%{
+#include "fl_interface.h"
+%}
---
+++ scratch/fake-lbann/test_1.py
@@ -0,0 +1,7 @@
+import os
+import fl_interface
+
+comm = os.getenv("COMM")
+print("comm: " + comm)
+
+fl_interface.fl_interface(int(comm), 3)
