0fd88d7f592e5e723b4fe2821da96128a408867e
Nick Collier <ncollier@anl.gov>
2019-01-16 13:04:53 -0500

    Addeded Yitan's additional FS functions

--- workflows/xcorr/xcorr.py
+++ workflows/xcorr/xcorr.py
@@ -1,8 +1,59 @@
 import numpy as np
+from scipy.stats import ttest_ind
+
 import os
 
 
-def compute_feature_correlationf(study1_f, study2_f, cutoff, delimiter='\t'):
+def ttest_feature_selection(data, labels, cutoff):
+    """
+    Use t-test to select features that are discriminative between two sample classes
+
+    :param data: an array, where rows are samples and columns are features (e.g., RNA expression row)
+    :param labels: a binary variable vector indicating the class labels, with length is 
+        the same as the number of rows in data. (e.g., normal or tumor)
+    :param cutoff: a positive number for selecting predictive features. If cutoff < 1, this function
+       selects features with p-values <= cutoff. If cutoff >= 1, it must be an integer
+       indicating the number of features to be selected based on p-value.
+    :returns a list of indices of the selected features.
+    """
+
+    unique_label = list(set(labels))
+    if len(unique_label) != 2:
+        raise ValueError('T-test feature selection needs two sample classes')
+
+    id0 = np.where(labels == unique_label[0])[0]
+    id1 = np.where(labels == unique_label[1])[0]
+    if len(id0) < 3 or len(id1) < 3:
+        raise ValueError('T-test feature selection requires every sample class has at least 3 samples')
+        
+    _, p = ttest_ind(a=data[id0, :], b=data[id1, :], axis=0, equal_var=False)
+    if cutoff < 1:
+        fid = np.where(p <= cutoff)[0]
+    else:
+        fid = sorted(range(len(p)), key=lambda x: p[x])[:int(cutoff)]
+    return sorted(fid)
+
+def correlation_feature_selection(data, targets, cutoff):
+    """
+    Use Pearson correlation coefficient to select predictive features for regression.
+    :param data: an array, where rows are samples and columns are features
+    :param targets: a vector of real numbers indicating the regression targets, with length the same as the       number of rows in data.
+    :param cutoff: a positive number for selecting predictive features. If cutoff < 1, this function selects
+       the features with an absolute correlation coefficient >= cutoff. If cutoff >= 1, it must be an
+       integer indicating the number of features to be selected based on absolute correlation coefficient.
+    :returns a list of indices of the selected features.
+    """
+
+    cor = np.corrcoef(np.vstack((np.transpose(data), np.reshape(targets, (1, len(targets))))))
+    cor = abs(cor[:-1, -1])
+    if cutoff < 1:
+        fid = np.where(cor >= cutoff)[0]
+    else:
+        fid = sorted(range(len(cor)), key=lambda x: cor[x], reverse=True)[:int(cutoff)]
+    return sorted(fid)
+
+
+def coxen_feature_selectionf(study1_f, study2_f, cutoff, delimiter='\t'):
     """ 
     Use the COXEN approach to select the features that are generalizable between 
     the data in study1_f and study2_f. study1_f and study2_f should have an equal number 
@@ -18,10 +69,10 @@ def compute_feature_correlationf(study1_f, study2_f, cutoff, delimiter='\t'):
     
     data1 = np.loadtxt(study1_f, delimiter=delimiter)
     data2 = np.loadtxt(study2_f, delimiter=delimiter)
-    return compute_feature_correlation(data1, data2, cutoff)
+    return coxen_feature_selection(data1, data2, cutoff)
     
 
-def compute_feature_correlation(data1, data2, cutoff):
+def coxen_feature_selection(data1, data2, cutoff):
     """ 
     Use the COXEN approach to select the features that are generalizable between data1 and data2.
     data1 and data2 should have an equal number of features. The features in data1 and data2 should 
