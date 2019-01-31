92b3d3bf10551ed38c8ba8a713b7dce4515e4608
Justin Wozniak <woz@anl.gov>
2019-01-31 15:10:36 -0600

    WS

--- workflows/xcorr/uno_xcorr.py
+++ workflows/xcorr/uno_xcorr.py
@@ -10,9 +10,9 @@ drug_df = None
 def init_uno_xcorr(rna_seq_path, drug_response_path, drug_ids=None):
     """Initialize this package for xcorr and the Uno benchmark
 
-    :param rna_seq_path: path to gene expression data following the format of 
+    :param rna_seq_path: path to gene expression data following the format of
         combined_rnaseq_data_combat
-    :param drug_response_path: path to drug response data following the format of 
+    :param drug_response_path: path to drug response data following the format of
         rescaled_combined_single_drug_growth
     """
 
@@ -20,7 +20,7 @@ def init_uno_xcorr(rna_seq_path, drug_response_path, drug_ids=None):
     gene_df = pd.read_csv(rna_seq_path, compression='infer', sep='\t', engine='c', na_values=['na', '-', ''],
         header=0, index_col=0)
     gene_df['study'] = gene_df.index.str.extract('^([^.]*)', expand=False)
-    
+
     global drug_df
     drug_df = pd.read_csv(drug_response_path, compression='infer', sep='\t', engine='c',
                 na_values=['na', '-', ''], header=0, index_col=None)
@@ -29,11 +29,11 @@ def init_uno_xcorr(rna_seq_path, drug_response_path, drug_ids=None):
 
 
 def select_features(df, study_col, study='all'):
-    """ Selects and returns a data frame from features whose 
+    """ Selects and returns a data frame from features whose
     study is equal to the specified study. If study is 'all' then
-    all features are returned. 
+    all features are returned.
 
-    :param study: a string specifing the study -- one of 'CCLE', 'CTRP', 'gCSI', 'GDSC', 'NCI60' 
+    :param study: a string specifing the study -- one of 'CCLE', 'CTRP', 'gCSI', 'GDSC', 'NCI60'
         or 'all'.
     """
 
@@ -44,7 +44,7 @@ def select_features(df, study_col, study='all'):
 
 
 ## TODO: add additional args / functions for additional sample selection
-def coxen_feature_selection(study_1, study_2, correlation_cutoff, 
+def coxen_feature_selection(study_1, study_2, correlation_cutoff,
     cross_correlation_cutoff, drug_ids=None, output_file=None):
 
     df1 = select_features(gene_df, 'study', study_1)
@@ -64,16 +64,16 @@ def coxen_feature_selection(study_1, study_2, correlation_cutoff,
 
     # perform the first step of COXEN approach to select predictive genes. To avoid exceeding the memory limit,
     # the prediction power of genes (i.e. absolute correlation coefficient with drug response) is calculated in batches.
-    gid1 = xcorr.correlation_feature_selection(df1, dr_df.GROWTH.values, 
+    gid1 = xcorr.correlation_feature_selection(df1, dr_df.GROWTH.values,
             dr_df.CELLNAME, correlation_cutoff)
-    
+
     # keep only predictive genes for data1 and data2
     df1 = df1.iloc[:, gid1]
     df2 = df2.iloc[:, gid1]
 
-    gid2 = xcorr.cross_correlation_feature_selection(df1.values, df2.values, 
+    gid2 = xcorr.cross_correlation_feature_selection(df1.values, df2.values,
         cross_correlation_cutoff)
-    
+
     genes = df1.columns[gid2]
     if output_file is not None:
         with open(output_file, 'w') as f_out:
