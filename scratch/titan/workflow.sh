30d928b95b44b5ac98b98c73dba63359eca11361
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-02-05 20:57:12 -0600

    Update paths

--- scratch/titan/workflow.sh
+++ scratch/titan/workflow.sh
@@ -8,11 +8,11 @@ then
 fi
 SCRIPT=$1
 
-LD_LIBRARY_PATH=/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3x/lib64/R/lib:/opt/java/jdk1.8.0_51/jre/lib/amd64/server:/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3/lib64/R/lib:/sw/xk6/curl/7.30.0/sles11.1_gnu4.3.4/lib:/opt/gcc/4.9.3/snos/lib64
+LD_LIBRARY_PATH=/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3x/lib64/R/lib:/opt/java/jdk1.8.0_51/jre/lib/amd64/server:/sw/xk6/r/3.3.2/sles11.3_gnu4.9.3/lib64/R/lib:/sw/xk6/curl/7.30.0/sles11.1_gnu4.3.4/lib:/opt/gcc/6.3.0/snos/lib64
 
-SWIFT=/lustre/atlas2/csc249/proj-shared/sfw/swift-t/stc/bin/swift-t
+SWIFT=/lustre/atlas2/med106/world-shared/sfw/titan/compute/swift-t/2018-12-12/stc/bin/swift-t
 
-export PROJECT=CSC249ADOA01
+export PROJECT=med106
 export QUEUE=debug
 export TITAN=true
 export PPN=2
