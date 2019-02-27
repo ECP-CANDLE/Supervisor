65142cf9c2cd51fdd59014808614d27d8e548c8d
brettin <brettin@anl.gov>
2019-02-27 19:14:56 +0000

    use module load datascience/keras-2.2.2 to get python

--- workflows/common/sh/langs-app-theta.sh
+++ workflows/common/sh/langs-app-theta.sh
@@ -6,7 +6,12 @@ export OMP_NUM_THREADS=128
 export NUM_INTER_THREADS=1
 export NUM_INTRA_THREADS=128
 
-export PYTHONHOME="/lus/theta-fs0/projects/Candle_ECP/ncollier/py2_tf_gcc6.3_eigen3_native"
+# export PYTHONHOME="/lus/theta-fs0/projects/Candle_ECP/ncollier/py2_tf_gcc6.3_eigen3_native"
+module load datascience/keras-2.2.2
+export PYTHONHOME="/opt/intel/python/2017.0.035/intelpython35/"
+export PYTHONPATH=$PYTHONPATH:/lus/theta-fs0/projects/CSC249ADOA01/hsyoo/candle_py_deps/
+
+
 PYTHON="$PYTHONHOME/bin/python"
 export LD_LIBRARY_PATH="$PYTHONHOME/lib"
 export PATH="$PYTHONHOME/bin:$PATH"
