08bdd6a76ab4607fdb9bc9d9cd00f2e503c5c062
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-02-28 14:41:05 -0600

    New Summit settings

---
+++ workflows/common/sh/langs-app-summit.sh
@@ -0,0 +1,14 @@
+
+# LANGS APP SUMMIT SH
+
+# WIP 2019-02-28
+
+PY=/gpfs/alpine/world-shared/med106/miniconda3
+
+# APP_PYTHONPATH=${APP_PYTHONPATH:-$PYTHONPATH}
+
+# Clear anything set by the system or Swift/T environment
+unset PYTHONPATH
+unset LD_LIBRARY_PATH
+
+# TODO ...
