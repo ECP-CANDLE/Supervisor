c85442e952682bc1bec94a1560b797ba604ccf3e
Nick Collier <ncollier@anl.gov>
2019-02-28 18:32:10 -0500

    Updated for summit

--- workflows/common/sh/env-summit.sh
+++ workflows/common/sh/env-summit.sh
@@ -12,7 +12,7 @@ ROOT=/gpfs/alpine/world-shared/med106
 export PY=$ROOT/miniconda3
 export R=$ROOT/gcc-6.4.0/R-3.5.2/lib64/R/lib
 
-export LD_LIBRARY_PATH=$R/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$R:$LD_LIBRARY_PATH
 
 SWIFT=$ROOT/gcc-6.4.0/swift-t/2019-02-27
 PATH=$SWIFT/stc/bin:$PATH
---
+++ workflows/cp1/test/cfg-prm-3.sh
@@ -0,0 +1,42 @@
+# CFG PRM 1
+
+# mlrMBO settings
+
+# Total runs/points/models per iteration
+PROPOSE_POINTS=${PROPOSE_POINTS:-3}
+# Total runs/points/models in first round
+DESIGN_SIZE=${DESIGN_SIZE:-3}
+
+MAX_CONCURRENT_EVALUATIONS=${MAX_CONCURRENT_EVALUATIONS:-3}
+MAX_ITERATIONS=${MAX_ITERATIONS:-3}
+MAX_BUDGET=${MAX_BUDGET:-30}
+
+CACHE_DIR=$EMEWS_PROJECT_ROOT/cache
+DB_FILE=$EMEWS_PROJECT_ROOT/cp1.db
+
+export PREPROP_RNASEQ="combat"
+
+# TODO: move the following code to a utility library-
+#       this is a configuration file
+# Set the R data file for running
+# if [ "$MODEL_NAME" = "combo" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/combo_hps_exp_01.R}
+# elif [ "$MODEL_NAME" = "p1b1" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b1_hps_exp_01.R}
+# elif [ "$MODEL_NAME" = "nt3" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/nt3_hps_exp_01.R}
+# elif [ "$MODEL_NAME" = "p1b3" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b3_hps_exp_01.R}
+# elif [ "$MODEL_NAME" = "p1b2" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b2_hps_exp_01.R}
+# elif [ "$MODEL_NAME" = "p2b1" ]; then
+#     PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p2b1_param1.R}
+# fi
+
+PARAM_SET_FILE=$EMEWS_PROJECT_ROOT/data/uno_quick_test.R
+
+if [[ "${PARAM_SET_FILE:-}" == "" ]]; then
+  # PARAM_SET_FILE must be set before this script returns!
+  echo "Invalid model-" "'${MODEL_NAME:-}'"
+  exit 1
+fi
