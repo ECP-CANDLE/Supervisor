dab503449427844dc4b3f60cc985d20b373dfe88
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-02-28 14:56:36 -0600

    Clean up: -cc none is now in env-theta

--- workflows/mlrMBO/swift/workflow.sh
+++ workflows/mlrMBO/swift/workflow.sh
@@ -126,10 +126,6 @@ then
   echo "Turbine will wait for job completion."
 fi
 
-# This should be moved to one or more specific site files.
-# It does not work on workstations, for example.  -Justin 2018/04/18
-# export TURBINE_LAUNCH_OPTIONS="-cc none"
-
 swift-t -n $PROCS \
         ${MACHINE:-} \
         -p -I $EQR -r $EQR \
