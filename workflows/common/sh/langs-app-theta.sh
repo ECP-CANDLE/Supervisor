612ad63f13d28cb4897ca60318c22e25b1274fd6
Nick Collier <ncollier@anl.gov>
2019-02-28 16:42:58 +0000

    WIP updates for running on theta

--- workflows/common/sh/langs-app-theta.sh
+++ workflows/common/sh/langs-app-theta.sh
@@ -6,10 +6,17 @@ export OMP_NUM_THREADS=128
 export NUM_INTER_THREADS=1
 export NUM_INTRA_THREADS=128
 
-# export PYTHONHOME="/lus/theta-fs0/projects/Candle_ECP/ncollier/py2_tf_gcc6.3_eigen3_native"
+# export PYTHONHOME="/lus/theta-fs
+APP_PYTHONPATH=${APP_PYTHONPATH:-$PYTHONPATH}
+
+unset PYTHONPATH
+unset LD_LIBRARY_PATH
+
 module load datascience/keras-2.2.2
-export PYTHONHOME="/opt/intel/python/2017.0.035/intelpython35/"
-export PYTHONPATH=/lus/theta-fs0/projects/CSC249ADOA01/hsyoo/candle_py_deps/
+export PYTHONHOME="/opt/intel/python/2017.0.035/intelpython35"
+
+APP_PYTHONPATH=${APP_PYTHONPATH:-$PYTHONPATH}
+PYTHONPATH+=":/lus/theta-fs0/projects/CSC249ADOA01/hsyoo/candle_py_deps/"
 
 
 PYTHON="$PYTHONHOME/bin/python"
@@ -19,5 +26,6 @@ export PATH="$PYTHONHOME/bin:$PATH"
 COMMON_DIR=$EMEWS_PROJECT_ROOT/../common/python
 PYTHONPATH+=":$PYTHONHOME/lib/python3.5:"
 PYTHONPATH+=":$COMMON_DIR:"
-PYTHONPATH+="$PYTHONHOME/lib/python3.5/site-packages"
+PYTHONPATH+="$PYTHONHOME/lib/python3.5/site-packages:$APP_PYTHONPATH"
+echo "APP PYTHONPATH: $PYTHONPATH"
 export PYTHONPATH
