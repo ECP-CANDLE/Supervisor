fd1d92d39f817107954682e1623e524855c3aed9
Justin Wozniak <woz@anl.gov>
2019-03-15 10:57:08 -0500

    Use foreign keys; fix to "list of value_ids"

--- workflows/cp1/db/db-hpo-init.sql
+++ workflows/cp1/db/db-hpo-init.sql
@@ -18,7 +18,7 @@ create table if not exists hpo_ids(
 create table if not exists hpo_hyperparam_defns(
        param_id integer primary key,
        /* reference to table hpo_ids */
-       hpo_id integer,
+       hpo_id integer foreign key hpo_ids(hpo_id),
        /* the name of the hyperparameter */
        name text
 );
@@ -26,10 +26,9 @@ create table if not exists hpo_hyperparam_defns(
 /* For each HPO instance there is a set of param_ids .
    Each categorical param_id has a set of values in this table */
 create table if not exists hpo_hyperparam_values(
+       value_id integer primary key,
        /* ID in table hpo_hyperparam_defns */
-       param_id integer,
-       /* ID in table hpo_ids */
-       hpo_id integer,
+       param_id integer foreign key hpo_hyperparam_defns(param_id),
        /* one of the possible valid values for this param_id */
        value text
 );
@@ -41,7 +40,7 @@ create table if not exists hpo_samples(
        creation timestamp,
        /* completion time */
        completion timestamp,
-       /* params: for categoricals, a comma-separated list of param_ids */
+       /* params: for categoricals, a comma-separated list of value_ids */
        hyperparams text,
        /* the sample value, e.g., val_loss */
        value real
