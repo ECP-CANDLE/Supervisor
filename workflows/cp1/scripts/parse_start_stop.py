139b2a462a1e3953bb0463a45b472dd7d7f5de34
Nick Collier <ncollier@anl.gov>
2019-03-26 11:41:10 -0400

    Updated result parsing

--- workflows/cp1/scripts/parse_start_stop.py
+++ workflows/cp1/scripts/parse_start_stop.py
@@ -7,6 +7,36 @@ TIME_FORMAT='%Y/%m/%d %H:%M:%S'
 START = 0
 STOP = 1
 
+def create_counts(timings_file):
+    hpos = {'all' : []}
+    with open(timings_file) as f_in:
+        reader = csv.reader(f_in)
+        for row in reader:
+            hpo_id = row[0]
+            if hpo_id in hpos:
+                hpos[hpo_id].append(row[1:])
+            else:
+                hpos[hpo_id] = [row[1:]]
+            hpos['all'].append(row[1:])
+
+    for k in hpos:
+        sorted(hpos[k], itemgetter(0))
+
+    counts = {'all' : []}
+    for k in hpos:
+        count = 0
+        for ts, ev in hpos[k]:
+            if ev == START:
+                count += 1
+            else:
+                count -= 1
+            
+            if k in counts:
+                counts[k].append([ts, count])
+            else:
+                counts[k] = [[ts, count]]
+
+
 def grep(model_log):
     output = subprocess.check_output(['grep', '-E', "RUN START|RUN STOP", model_log])
     lines = output.decode("utf-8")
@@ -15,7 +45,7 @@ def grep(model_log):
         idx = line.find(' __main')
         if idx != -1:
             ts = line[0:idx]
-            dt = datetime.datetime.strptime(ts, TIME_FORMAT)
+            dt = datetime.datetime.strptime(ts, TIME_FORMAT).timestamp()
             if line.endswith('START'):
                 result.append((dt, START))
             else:
@@ -24,30 +54,27 @@ def grep(model_log):
     return result
 
 def write_results(results):
-    for hpo_id in results:
-        with open('{}_timings.txt'.format(hpo_id), 'w') as f_out:
-            result = results[hpo_id]
-            for r in result:
-                f_out.write('{} {}'.format(r[0], r[1]))
+    with open('timings.txt', 'w') as f_out:
+        
+        result = results[hpo_id]
+        for r in result:
+            for i in r:
+                f_out.write('{} {}\n'.format(i[0], i[1]))
 
 def main(hpos_file):
     results = {}
-    with open(hpos_file) as f_in:
-        reader = csv.reader(f_in, delimiter='|')
-        for row in reader:
-            hpo_id = row[1]
-            run_dir = row[3]
-            result = grep('{}/model.log'.format(run_dir))
-            if len(result) > 0:
-                if hpo_id in results:
-                    results[hpo_id].append(result)
-                else:
-                    results[hpo_id] = [result]
-    
+    with open('timings.txt', 'w') as f_out:
+        with open(hpos_file) as f_in:
+            reader = csv.reader(f_in, delimiter='|')
+            for i, row in enumerate(reader):
+                if i % 1000 == 0:
+                    print('ROW: {}'.format(i))
+                hpo_id = row[1]
+                run_dir = row[3]
+                result = grep('{}/model.log'.format(run_dir))
+                for r in result:
+                    f_out.write('{},{},{}\n'.format(hpo_id, r[0], r[1]))
 
-
-
-            
 
 if __name__ == "__main__":
-    grep(sys.argv[1])
+    main(sys.argv[1])
