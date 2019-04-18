9248130686ab742b24b19456dcedab688206f576
Justin Wozniak <woz@anl.gov>
2019-04-18 14:21:04 -0500

    Clean up

--- scratch/csv2f64/csv2f64.c
+++ scratch/csv2f64/csv2f64.c
@@ -72,10 +72,10 @@ static bool
 convert_fps(FILE* fp_i, FILE* fp_o)
 {
   size_t bytes_c = buffer_size * sizeof(char);
-  char* chars = malloc(bytes_c);
+  char* chars = malloc(bytes_c); // the input buffer
   CHECK(chars != NULL, "could not allocate memory: %zi", bytes_c);
   size_t bytes_f = buffer_size * sizeof(double);
-  double* floats = malloc(bytes_f);
+  double* floats = malloc(bytes_f); // the output buffer
   CHECK(floats != NULL, "could not allocate memory: %zi", bytes_f);
 
   int cols_last = -1; // number of columns in last row
@@ -85,7 +85,7 @@ convert_fps(FILE* fp_i, FILE* fp_o)
   int offset = 0; // starting offset in chars due to chars from last read
   size_t actual_r = 0; // actual number of items read at last fread
   int length = 0; // the length of our current data (offset+actual_r)
-  int w = 0; // word start
+  int w = 0; // index of word start
   bool data_on_line = false; // is there good data on this line?
   bool b;
   int i = 0;
@@ -97,29 +97,22 @@ convert_fps(FILE* fp_i, FILE* fp_o)
     length = offset + actual_r;
     for (i = 0; i < length; i++)
     {
-      // printf("chars[i=%i]='%c'\n", i, chars[i]);
       if (chars[i] == ' ' || chars[i] == '\t') continue; // ignore WS
       if (chars[i] == '\n' && ! data_on_line) continue; // blank line
       data_on_line = true; // found good data - not a blank line
       if (chars[i] == ',' || chars[i] == '\n') // end of word
       {
-        if (i == w)
-          // Word length is 0 and not blank line - fail
-          FAIL("bad text on line: %i column: %i", rows, i-offset+1);
-        // printf("chars[w=%i]=%c\n", w, chars[w]);
+	// If word length is 0 and not a blank line: fail
+        CHECK(i-w > 0, "bad text on line: %i column: %i", rows, i-offset+1);
         b = convert_word(rows, &chars[w], floats, &f, fp_o);
         w = i+1;
       }
       b = update_rows_cols(chars[i], &cols, &cols_last, &rows, &data_on_line);
       CHECK(b, "bad input!");
     }
-    // Out of space in chars: copy remaining data (partial words)
-    // to front of buffer
-    // printf("copy: w=%i\n", w);
+    // Out of space in chars: copy partial words to front of buffer
     offset = length - w;
     memcpy(chars, &chars[w], offset);
-    // chars[offset] = '\0';
-    // printf("char start: '%s'\n", chars);
     w = 0;
   }
 
@@ -145,7 +138,6 @@ static inline bool
 read_text(char* data, size_t count, FILE* fp, size_t* actual_r)
 {
   size_t actual = fread(data, sizeof(char), count, fp);
-  // printf("actual_r: %zi\n", actual);
   if (actual == 0)
     CHECK(!ferror(fp), "read failed!");
   *actual_r = actual;
@@ -165,7 +157,6 @@ convert_word(int rows, const char* word_start,
   errno = 0;
   floats[fi] = strtod(word_start, NULL);
   CHECK(errno == 0, "bad number on line: %i", rows);
-  // printf("f[%i]=%f\n", fi, floats[fi]);
   fi++;
 
   if (fi == buffer_size)
@@ -191,7 +182,6 @@ update_rows_cols(char c, int* cols, int* cols_last,
   if (c == '\n')
   {
     (*rows)++;
-    // printf("cols=%i cols_last=%i\n", cols, cols_last);
     CHECK(*cols == *cols_last || *cols_last == -1,
           "bad column count on line: %i cols=%i cols_last=%i",
           *rows, *cols, *cols_last);
