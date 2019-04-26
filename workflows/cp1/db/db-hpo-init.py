92ae3ec00bc2480682a2fc0f6f34e23f267d8a82
Justin Wozniak <woz@anl.gov>
2019-04-26 13:30:26 -0500

    Accept DB file as argument

--- workflows/cp1/db/db-hpo-init.py
+++ workflows/cp1/db/db-hpo-init.py
@@ -4,11 +4,8 @@
 # See db-hpo-init.sql for the table schema
 
 import os, sys
-import yaml
 
-from xcorr_db import xcorr_db, q
-
-DB = xcorr_db('xcorr.db', log=False)
+from xcorr_db import xcorr_db
 
 def create_tables(db_hpo_init_sql):
     """ Set up the tables defined in the SQL file """
@@ -22,10 +19,20 @@ def create_tables(db_hpo_init_sql):
 #     DB.execute("create index features_index on features(record_id);")
 #     DB.execute("create index  studies_index on studies ( study_id);")
 
+def get_args(argv):
+    import argparse
+    parser = argparse.ArgumentParser(description=
+                                     'Setup the DB for CP1 HPOs.')
+    parser.add_argument('db_filename',       help='The DB file name')
+    args = parser.parse_args()
+    return args
+
 # Catch and print all exceptions to improve visibility of success/failure
 success = False
 try:
     this = os.getenv("THIS")
+    args = get_args(sys.argv)
+    DB = xcorr_db(args.db_filename, log=False)
     db_hpo_init_sql = this + "/db-hpo-init.sql"
     create_tables(db_hpo_init_sql)
     success = True
