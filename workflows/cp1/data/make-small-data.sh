1ff4c9cca54a5d6bc67fe13e639674f05639d25c
Justin Wozniak <woz@anl.gov>
2019-04-26 13:29:49 -0500

    Finish creating studies-features files

--- workflows/cp1/data/make-small-data.sh
+++ workflows/cp1/data/make-small-data.sh
@@ -6,13 +6,14 @@ set -eu
 # See the README for data provenance
 
 # INPUT:  data/studies*.txt
-#         data/combined_rnaseq_data_combat
-# OUTPUT: data/features-small.txt
-#         data/rnaseq-small.txt
+#         data/combined_rnaseq_data_combat ORIG
+# OUTPUT: data/features-small.txt          FEATURES_SMALL
+#         data/rnaseq-small.txt            RNA_SMALL
 
 # Configurable variables:
 ORIG=data/combined_rnaseq_data_combat
-SMALL=data/rnaseq-small.txt
+RNA_SMALL=data/rnaseq-small.txt
+FEATURES_SMALL=data/features-small.txt
 N_FEATURES=3
 N_SAMPLES=5
 # End configurable variables.
@@ -20,13 +21,42 @@ N_SAMPLES=5
 TAB="$( echo -ne "\t" )" # Tab special character
 
 COLS=$(( $N_FEATURES + 1 )) # Include "Sample" token
-cut -d "$TAB" -f 1-$COLS $ORIG | head -1 > data/features-small.txt
+cut --delimiter="$TAB" --fields=1-$COLS $ORIG | \
+  head --lines 1 > $FEATURES_SMALL
 
-STUDIES=( $( sed 's/#.*//' data/studies*.txt | sort -u ) ) # Remove comments
+# Use sed to remove comments, extract each unique study
+STUDIES=( $( sed 's/#.*//' data/studies*.txt | sort --unique ) )
 
 # echo ${STUDIES[@]}
 
+# For each study, extract the first COLS columns of the N_SAMPLES rows
 for STUDY in ${STUDIES[@]}
 do
-  grep --max-count=$N_SAMPLES $STUDY $ORIG | cut -d "$TAB" -f 1-$COLS
-done > $SMALL
+  grep --max-count=$N_SAMPLES $STUDY $ORIG | \
+    cut --delimiter="$TAB" --fields=1-$COLS
+done > $RNA_SMALL
+
+# Create CCLE_CTRP_2000_1000_features_dummy.txt
+#   with feature names
+# That is, for each STUDY1, STUDY2, CUTOFF1, CUTOFF2:
+#   create STUDY1_STUDY2_CUTOFF1_CUTOFF2_features_small.txt
+#   containing some random subset of the features
+FEATURES=( $( cut --delimiter="$TAB" --fields=2- $FEATURES_SMALL ) )
+echo ${FEATURES[@]}
+for STUDY1 in ${STUDIES[@]}
+do
+  for STUDY2 in ${STUDIES[@]}
+  do
+    if [[ $STUDY1 != $STUDY2 ]]
+    then
+      FEATURES_LIST=xcorr_data/${STUDY1}_${STUDY2}_2000_1000_features_small.txt
+      for F in ${FEATURES[@]}
+      do
+        if (( $RANDOM % 2 == 0 ))
+        then
+          echo "rnaseq.$F"
+        fi
+      done > $FEATURES_LIST
+    fi
+  done
+done
