444e8c6249e326d1bbd82946efa4928723160606
Justin Wozniak <woz@anl.gov>
2019-05-24 14:09:19 -0500

    Value flipper for f64

--- scratch/csv2f64/inject-noise.py
+++ scratch/csv2f64/inject-noise.py
@@ -15,12 +15,19 @@ args = parser.parse_args(sys.argv[1:])
 
 print(args.file, args.rate)
 
-a8 = np.fromfile(args.file, dtype='float64')
-print(a8.shape)
-
 rate = float(args.rate)
+
+a8 = np.fromfile(args.file, dtype='float64')
+print('input size: ', a8.shape[0])
+print('flip pct:   ', rate, '%')
+
+rate = rate / 100
+flips = 0
+
 for i in range(0, a8.shape[0]):
     if random.random() < rate:
         a8[i] = -a8[i]
+        flips += 1
 
 a8.tofile(args.file)
+print('flipped:    ', flips)
