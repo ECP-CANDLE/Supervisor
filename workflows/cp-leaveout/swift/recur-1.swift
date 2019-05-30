9bb74aa35cad0c63ce42786d9875395dfe3e4023
Justin Wozniak <woz@anl.gov>
2019-05-30 13:40:04 -0500

    Simplify

--- workflows/cp-leaveout/swift/recur-1.swift
+++ workflows/cp-leaveout/swift/recur-1.swift
@@ -1,4 +1,10 @@
 
+/*
+  RECUR 1 SWIFT
+
+  Simply run with: 'swift-t recur-1.swift'
+*/
+
 app (void v) dummy(string parent, int stage, int id, void block)
 {
   "echo" ("parent='%3s'"%parent) ("stage="+stage) ("id="+id) ;
@@ -7,17 +13,6 @@ app (void v) dummy(string parent, int stage, int id, void block)
 N = 4;
 
 void A[string];
-
-int stage = 1;
-foreach id in [0:N-1]
-{
-  string this = int2string(id);
-  v = dummy("", 2, id, propagate());
-  foreach id_child in [0:N-1]
-  {
-    runstage(N, this, stage+1, id_child, v);
-  }
-}
 
 (void v) runstage(int N, string parent, int stage, int id, void block)
 {
@@ -30,4 +25,10 @@ foreach id in [0:N-1]
       runstage(N, this, stage+1, id_child, v);
     }
   }
+}
+
+int stage = 1;
+foreach id in [0:N-1]
+{
+  runstage(N, "", stage, id, propagate());
 }
