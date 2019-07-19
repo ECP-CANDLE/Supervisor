b9b058f213e9d7fdec11b324dd84df54cd491842
Justin Wozniak <woz@anl.gov>
2019-07-19 15:15:49 -0500

    Move link creation into data_setup: resolves #62

--- workflows/cp-leaveout/py/data_setup.py
+++ workflows/cp-leaveout/py/data_setup.py
@@ -2,6 +2,7 @@
 # DATA SETUP PY
 
 import json
+import os
 
 from runner_utils import ModelResult
 import topN_to_uno
@@ -18,6 +19,14 @@ def pre_run(params):
                      params["node"],
                      params["plan"])
     print("topN build node: '%s' ..." % params["node"])
+    data = params["benchmark_data"]
+    try:
+        for filename in [ "cache", "uno_auc_model.txt" ]:
+            if not os.path.islink(filename):
+                os.symlink(f"{data}/{filename}", filename)
+    except Exception as e:
+        print("data_setup: error making symlink: %s\n" % filename + str(e))
+        return ModelResult.ERROR
     try:
         topN_to_uno.build_dataframe(args)
     except Exception as e:
