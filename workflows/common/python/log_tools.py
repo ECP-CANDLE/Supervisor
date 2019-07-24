a5b0f1273f666fdf1ef196d4b507283230105680
Justin Wozniak <woz@anl.gov>
2019-07-24 15:15:26 -0500

    Support new stream argument- untested

--- workflows/common/python/log_tools.py
+++ workflows/common/python/log_tools.py
@@ -3,14 +3,14 @@
 
 # Standardize some Python logging techniques
 
-def get_logger(logger, name):
+def get_logger(logger, name, stream=sys.stdout):
     """ Set up logging """
     if logger is not None:
         return logger
     import logging, sys
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    h = logging.StreamHandler(stream=sys.stdout)
+    h = logging.StreamHandler(stream=stream)
     fmtr = logging.Formatter('%(asctime)s %(name)s %(levelname)-9s %(message)s',
                              datefmt='%Y-%m-%d %H:%M:%S')
     h.setFormatter(fmtr)
