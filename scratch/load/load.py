4c7584330fd7afea1c689a39aa86cab455176aae
Justin Wozniak <woz@anl.gov>
2019-07-25 15:15:20 -0500

    New load plotter

--- scratch/load/load.py
+++ scratch/load/load.py
@@ -1,6 +1,8 @@
 
 # LOAD PY
 
+# Convert the date/time markers in the extracted start/stop times
+# into plottable data
 
 from datetime import datetime
 import sys
@@ -15,7 +17,8 @@ if len(sys.argv) != 3:
 def parse(d):
     return datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
 
-def emit(d, load):
+def emit(d, old_load, load):
+    print("%0.2f %03i" % (d.timestamp() - ts_start, old_load))
     print("%0.2f %03i" % (d.timestamp() - ts_start, load))
 
 start = parse(sys.argv[1])
@@ -23,18 +26,19 @@ stop =  parse(sys.argv[2])
 
 ts_start = start.timestamp()
 
-emit(start, load)
-    
+emit(start, load, load)
+
 for line in sys.stdin:
     # line = line.rstrip()
     tokens = line.split()
     date_string = tokens[0] + " " + tokens[1]
     d = parse(date_string)
     # print(date.timestamp())
+    old_load = load
     if "START" in tokens:
         load += 1
     if "STOP" in tokens:
         load -= 1
-    emit(d, load)
-    
-emit(stop, load)
+    emit(d, old_load, load)
+
+# emit(stop, load, load)
