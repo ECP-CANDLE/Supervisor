0b2715308beedcae9ee05b7c18af21e9206de0ba
Justin Wozniak <woz@anl.gov>
2019-07-25 11:04:36 -0500

    Add import sys

--- workflows/common/python/log_tools.py
+++ workflows/common/python/log_tools.py
@@ -3,11 +3,13 @@
 
 # Standardize some Python logging techniques
 
+import sys
+
 def get_logger(logger, name, stream=sys.stdout):
     """ Set up logging """
     if logger is not None:
         return logger
-    import logging, sys
+    import logging
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     h = logging.StreamHandler(stream=stream)
