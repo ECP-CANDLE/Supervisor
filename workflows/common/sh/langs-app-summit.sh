8406005d73baa91306f46eba16116fff8bb29457
Justin Wozniak <woz@anl.gov>
2019-07-29 14:26:22 -0500

    Add APP_PYTHONPATH

--- workflows/common/sh/langs-app-summit.sh
+++ workflows/common/sh/langs-app-summit.sh
@@ -14,17 +14,15 @@ module load cuda/9.2.148
 
 module list
 
-log_path PATH
-log_path LD_LIBRARY_PATH
-
 export PATH=/gpfs/alpine/world-shared/med106/miniconda3/bin:$PATH
 export LD_LIBRARY_PATH=/gpfs/alpine/world-shared/med106/miniconda3/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$APP_PYTHONPATH
 
 echo LANGS APP
-set -x
 
 which python3
 log_path PATH
 log_path LD_LIBRARY_PATH
-python3 -c "print('HI')"
+log_path PYTHONPATH
+python3 -c "print('HELLO WORLD')"
 set +x
