822c6f96d9c6640265cf0facebdeae20b84374d8
Gounley, John P <gounleyjp@ornl.gov>
2019-10-02 15:46:49 -0400

    Add example for 8 nodes

---
+++ workflows/mlrMBO/test/cfg-prm-8.sh
@@ -0,0 +1,38 @@
+# CFG PRM 1
+
+# mlrMBO settings
+
+# Total iterations
+PROPOSE_POINTS=${PROPOSE_POINTS:-46}
+MAX_ITERATIONS=${MAX_ITERATIONS:-3}
+MAX_BUDGET=${MAX_BUDGET:-138}
+DESIGN_SIZE=${DESIGN_SIZE:-46}
+# TODO: move the following code to a utility library-
+#       this is a configuration file
+# Set the R data file for running
+
+set -x
+if [ "$MODEL_NAME" = "combo" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/combo_hps_exp_01.R}
+elif [ "$MODEL_NAME" = "p1b1" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b1_hps_exp_01.R}
+elif [ "$MODEL_NAME" = "nt3" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/nt3_hps_exp_01.R}
+elif [ "$MODEL_NAME" = "p1b3" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b3_hps_exp_01.R}
+elif [ "$MODEL_NAME" = "p1b2" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p1b2_hps_exp_01.R}
+elif [ "$MODEL_NAME" = "p2b1" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p2b1_param1.R}
+elif [ "$MODEL_NAME" = "p3b3" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p3b3_param.R}
+elif [ "$MODEL_NAME" = "p3b4" ]; then
+    PARAM_SET_FILE=${PARAM_SET_FILE:-$EMEWS_PROJECT_ROOT/data/p3b4_param.R}
+fi
+
+if [[ "${PARAM_SET_FILE:-}" == "" ]]; then
+  # PARAM_SET_FILE must be set before this script returns!
+  echo "Invalid model-" "'${MODEL_NAME:-}'"
+  exit 1
+fi
+set +x
