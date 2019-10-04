b0e1989c9e6ef56995926fa44404bc6a4e183917
Justin Wozniak <woz@anl.gov>
2019-10-04 09:55:06 -0500

    Use NT3 by default

--- workflows/upf/test/upf-1.sh
+++ workflows/upf/test/upf-1.sh
@@ -17,7 +17,7 @@ EMEWS_PROJECT_ROOT=$( cd $THIS/..        ; /bin/pwd )
 WORKFLOWS_ROOT=$(     cd $THIS/../..     ; /bin/pwd )
 export EMEWS_PROJECT_ROOT
 
-export MODEL_NAME="p1b1" OBJ_RETURN="val_loss"
+export MODEL_NAME="nt3" OBJ_RETURN="val_loss"
 CFG_SYS=$THIS/cfg-sys-1.sh
 
 $EMEWS_PROJECT_ROOT/swift/workflow.sh $SITE -a $CFG_SYS $THIS/upf-1.txt
