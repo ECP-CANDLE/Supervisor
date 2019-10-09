893f9ebf98ad699099784b1aebe46daf58f6e940
Justin Wozniak <woz@anl.gov>
2019-10-09 16:02:50 -0500

    Improve obj_echo()

--- workflows/common/swift/obj_echo.swift
+++ workflows/common/swift/obj_echo.swift
@@ -8,12 +8,13 @@
     params : The JSON string of params to be passed to the Benchmark
     run_id : A string run ID that will be the output directory name
 */
-(string obj_result) obj(string params,
+(string obj_result) obj(string params_in,
                         string run_id) {
   string model_sh       = getenv("MODEL_SH");
   string turbine_output = getenv("TURBINE_OUTPUT");
 
   string outdir = "%s/run/%s" % (turbine_output, run_id);
+  params = replace_all(params_in, "\n", "", 0);
   //                                              1       2       3
   printf("bash model.sh %s %s %s in: %s", FRAMEWORK, params, run_id,
          turbine_output) =>
