479d1a1f5291eca5b4e50d076c44cf03388792df
Nick Collier <ncollier@anl.gov>
2019-10-21 17:18:14 -0400

    Moved chaining workflow files into cp-leaveout

---
+++ workflows/cp-leaveout/test-chained/cfg-sys-s1.sh
@@ -0,0 +1,61 @@
+
+# UPF CFG SYS 1
+
+# The number of MPI processes
+# Note that 1 processes is reserved for Swift/T
+# For example, if PROCS=4 that gives you 3 workers,
+# i.e., 3 concurrent Keras runs.
+export PROCS=${PROCS:-5}
+
+# MPI processes per node.  This should not exceed PROCS.
+export TURBINE_LAUNCH_OPTIONS="-a1 -c42 -g1"
+export PPN=${PPN:-1}
+
+# Uncomment when 1 node per run
+export TURBINE_DIRECTIVE+="\n#BSUB -alloc_flags \"NVME maximizegpfs\"\n"   
+
+#export QUEUE=${QUEUE:-batch}
+
+# Cori: (cf. sched-cori)
+# export QUEUE=${QUEUE:-debug}
+# Cori queues: debug, regular
+# export QUEUE=regular
+# export QUEUE=debug
+# CANDLE on Cori:
+# export PROJECT=m2924
+
+# Theta: (cf. sched-theta)
+# export QUEUE=${QUEUE:-debug-cache-quad}
+# export PROJECT=${PROJECT:-ecp-testbed-01}
+# export PROJECT=Candle_ECP
+# export PROJECT=CSC249ADOA01
+
+# Summit:
+# export QUEUE=${QUEUE:-batch}
+# export PROJECT=med106
+
+# Benchmark run timeout: benchmark run will timeouT                                                                                             
+# after the specified number of seconds. -1 is no timeout.  
+BENCHMARK_TIMEOUT=${BENCHMARK_TIMEOUT:--1}
+
+# Uncomment below to use custom python script to run
+# Use file name without .py (e.g, my_script.py)
+# MODEL_PYTHON_SCRIPT=my_script
+
+# Shell timeout: benchmark run will be killed
+# after the specified number of seconds.
+# If set to -1 or empty there is no timeout.
+# This timeout is implemented with the shell command 'timeout'
+export SH_TIMEOUT=${SH_TIMEOUT:-}
+
+
+# Ignore errors: If 1, unknown errors will be reported to model.log
+# but will not bring down the Swift workflow.  See model.sh .
+export IGNORE_ERRORS=0
+
+
+export WALLTIME=${WALLTIME:-02:00:00}
+
+# export MAIL_ENABLED=1
+# export MAIL_ADDRESS=wozniak@mcs.anl.gov
+
---
+++ workflows/cp-leaveout/test-chained/test-1.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -eu
+
+# CP LEAVEOUT CHAIN UPF TEST 1
+
+module unload python
+
+usage()
+{
+  echo "Usage: test SITE EXPID WORKFLOW_ARGS"
+}
+
+if (( ${#} == 0 ))
+then
+  usage
+  exit 1
+fi
+
+SITE=$1
+RUN_DIR=$2
+shift 2
+WORKFLOW_ARGS=$*
+
+export MODEL_NAME=uno #model
+
+# Self-configure
+THIS=$( cd $( dirname $0 ) && /bin/pwd )
+EMEWS_PROJECT_ROOT=$( cd $THIS/.. && /bin/pwd )
+export EMEWS_PROJECT_ROOT
+WORKFLOWS_ROOT=$( cd $EMEWS_PROJECT_ROOT/.. && /bin/pwd )
+source $WORKFLOWS_ROOT/common/sh/utils.sh
+
+# Select configurations
+export CFG_SYS=$THIS/$1
+export CFG_PRM=$THIS/cfg-prm-1.sh
+
+# Data files
+# PLAN_JSON=$EMEWS_PROJECT_ROOT/plangen_cell8-p2_drug8-p2.json
+SCRATCH=/gpfs/alpine/med106/scratch/ncollier/job-chain
+CANDLE_DATA=$SCRATCH/inputs
+PLAN_JSON=$2
+UPF_FILE=$3
+STAGE=$4
+PARENT_STAGE_DIRECTORY=$5
+DATAFRAME_CSV=$CANDLE_DATA/top_21.res_reg.cf_rnaseq.dd_dragon7.labled.feather
+# Override default of shared parent directory with Supervisor
+# This is necessary as benchmarks must be writeable from a
+# compute node
+export BENCHMARKS_ROOT=$SCRATCH/Benchmarks
+BENCHMARK_DATA=$BENCHMARKS_ROOT/Pilot1/Uno
+
+export TURBINE_DIRECTIVE=$6
+
+
+# What to return from the objective function (Keras model)
+# val_loss (default) and val_corr are supported
+export OBJ_RETURN="val_loss"
+
+if [[ $SITE == "theta" ]]
+then
+  export WAIT=1
+fi
+
+for f in $DATAFRAME_CSV $PLAN_JSON
+do
+  if ! [[ -f $f ]]
+  then
+    echo "$0: does not exist: $f"
+    exit 1
+  fi
+done
+
+# Submit job
+$EMEWS_PROJECT_ROOT/swift/cpl-upf-workflow.sh $SITE $RUN_DIR $CFG_SYS $CFG_PRM \
+                                      $MODEL_NAME $WORKFLOW_ARGS       \
+                                      --plan_json=$PLAN_JSON           \
+                                      --dataframe_csv=$DATAFRAME_CSV   \
+                                      --benchmark_data=$BENCHMARK_DATA \
+                                      --stage=$STAGE \
+                                      --parent_stage_directory=$PARENT_STAGE_DIRECTORY \
+                                      --f=$UPF_FILE
+
+# Check job output
+TURBINE_OUTPUT=$( readlink turbine-output )
+OUTPUT=turbine-output/output.txt
+WORKFLOW=$( basename $EMEWS_PROJECT_ROOT )
+
+# Wait for job
+#queue_wait
+
+SCRIPT=$( basename $0 .sh )
+#check_output "RESULTS:"     $OUTPUT $WORKFLOW $SCRIPT $JOBID
+#check_output "EXIT CODE: 0" $OUTPUT $WORKFLOW $SCRIPT $JOBID
+
+echo "$SCRIPT: SUCCESS"
+
+# Local Variables:
+# c-basic-offset: 2;
+# End:
