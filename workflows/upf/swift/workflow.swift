5de9ddec5a3937bfe2e1e03e668cf2149d4a35b3
Justin Wozniak <woz@anl.gov>
2019-11-01 13:50:44 -0500

    New environment reporting function in UPF for configuration debugging

--- workflows/upf/swift/workflow.swift
+++ workflows/upf/swift/workflow.swift
@@ -11,11 +11,58 @@ import files;
 import string;
 import sys;
 
+report_env() "turbine" "1.0"
+[
+----
+puts ""
+puts "report_env() ..."
+puts ""
+global env
+# puts [ array names env ]     
+puts "TURBINE_HOME: $env(TURBINE_HOME)"
+puts ""
+set tokens [ split $env(PATH) ":" ]
+foreach token $tokens {
+  puts "PATH: $token"
+}
+puts ""
+set tokens [ split $env(LD_LIBRARY_PATH) ":" ]
+foreach token $tokens {
+  puts "LLP: $token"
+}
+if [ info exists env(PYTHONHOME) ] {
+  puts ""
+  puts "PYTHONHOME: $env(PYTHONHOME)"
+}
+if [ info exists env(PYTHONPATH) ] {
+  puts ""
+  set tokens [ split $env(PYTHONPATH) ":" ]
+  foreach token $tokens {
+    puts "PYTHONPATH: $token"
+  }
+}
+puts ""
+set pythons [ exec which python python3 ]
+foreach p $pythons {
+  puts "PYTHON: $p"
+}
+puts ""
+puts "report_env() done."
+puts ""
+----
+];
+
+report_env();
+
 string FRAMEWORK = "keras";
 
 // Scan command line
 file   upf        = input(argv("f"));
 int    benchmark_timeout = toint(argv("benchmark_timeout", "-1"));
+
+string model_name     = getenv("MODEL_NAME");
+string exp_id         = getenv("EXPID");
+string turbine_output = getenv("TURBINE_OUTPUT");
 
 // Report some key facts:
 printf("UPF: %s", filename(upf));
@@ -33,7 +80,9 @@ foreach params,i in upf_lines
   printf("params: %s", params);
   id = json_get(params, "id");
   // NOTE: obj() is in the obj_*.swift supplied by workflow.sh
+  // id = "id_%02i"%i;
   results[i] = obj(params, id);
+  assert(results[i] != "EXCEPTION", "exception in obj()!");
 }
 
 // Join all result values into one big semicolon-delimited string
