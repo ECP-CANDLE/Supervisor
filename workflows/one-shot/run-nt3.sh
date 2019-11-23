0ef4e79166516142fed721872648f51dd32f3b6e
Justin M Wozniak <wozniak@mcs.anl.gov>
2019-11-23 11:26:48 -0600

    Clean up

--- workflows/one-shot/run-nt3.sh
+++ workflows/one-shot/run-nt3.sh
@@ -2,7 +2,7 @@
 # Need -l to reset modules ...
 set -eu
 
-# SETUP NT3
+# RUN NT3
 
 echo $( basename $0 )
 hostname
@@ -13,14 +13,13 @@ module load   datascience/tensorflow-1.14
 module load   datascience/keras-2.2.4
 # Modules end
 
+which python
+
 # Report original source directory
 echo THIS=$THIS
 
-set -x
 BENCHMARKS=$( readlink --canonicalize $THIS/../../../Benchmarks )
 NT3=$BENCHMARKS/Pilot1/NT3/nt3_baseline_keras2.py
-
-which python
 
 set -x
 python $NT3 --epochs 1
