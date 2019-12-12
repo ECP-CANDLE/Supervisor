a02db94c1a7a22be3630033328ca130b8eae4696
Justin Wozniak <woz@anl.gov>
2019-12-12 17:57:49 -0600

    Add timing and better messaging

--- workflows/cp-leaveout/py/data_setup.py
+++ workflows/cp-leaveout/py/data_setup.py
@@ -20,7 +20,7 @@ class TopN_Args:
 
 def pre_run(params):
     import sys, time
-    print("data_setup.pre_run()...")
+    print("data_setup.pre_run(): node: '%s' ..." % params["node"])
     sys.stdout.flush()
 
     # check NVMe disk is available
@@ -34,28 +34,30 @@ def pre_run(params):
             dest = Path("/mnt/bb/{}/{}".format(username, src.name))
             if not dest.exists():
                 start = time.time()
-                dest.write_bytes(src.read_bytes())
+                count = dest.write_bytes(src.read_bytes())
                 stop = time.time()
+                duration = stop - start
+                rate = count / duration / (1024*1024)
                 print("File copy completed. Original dataframe " +
-                      "copied to NVM in %0i seconds.\n" %
-                      (stop-start))
+                      "copied to NVM in %0.1f seconds (%0.1f MB/s)." %
+                      (duration, rate))
             else:
-                print("File copy skipped. Original dataframe already exists in NVM.\n")
+                print("File copy skipped. Original dataframe already exists in NVM.")
         except Exception as e:
             print("Error occurred in copying original dataframe\n" + str(e))
             traceback.print_exc()
             return ModelResult.ERROR
         params["dataframe_from"] = dest.resolve()
         params["use_exported_data"] = "/mnt/bb/{}/{}".format(username, params["use_exported_data"])
-
+        
     # softlink to cache & config file
-    # build node specific training/validatoin dataset
+    # build node specific training/validation dataset
+    
     args = TopN_Args(params["dataframe_from"],
                      params["node"],
                      params["plan"],
                      params["use_exported_data"])
 
-    print("topN build node: '%s' ..." % params["node"])
     data = params["benchmark_data"]
     try:
         for filename in [ "cache", "uno_auc_model.txt" ]:
@@ -66,8 +68,17 @@ def pre_run(params):
         return ModelResult.ERROR
 
     try:
+        print("data_setup: build_dataframe() ...")
+        start = time.time()
         topN_to_uno.build_dataframe(args)
+        stop = time.time()
+        duration = stop - start
+        print("data_setup: build_dataframe() OK : " +
+              "%0.1f seconds." % duration)
     except ValueError:
+        print("data_setup: caught ValueError for node: '%s'" %
+              params["node"]) # new 2019-12-02
+        traceback.print_exc(file=sys.stdout)
         return ModelResult.SKIP
     except Exception as e:
         print("data_setup: error in build_dataframe!\n" + str(e))
