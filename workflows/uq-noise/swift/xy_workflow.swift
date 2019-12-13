27045f7e233ac6e0a19b9d4d317afcbf0697cbc1
Rajeev Jain <rajeeja@gmail.com>
2019-12-12 23:35:00 -0600

    adding xy noise

---
+++ workflows/uq-noise/swift/xy_workflow.swift
@@ -0,0 +1,56 @@
+/*
+  UQ NOISE SWIFT
+  Main workflow
+*/
+
+import assert;
+import files;
+import io;
+import python;
+import unix;
+import sys;
+import string;
+import location;
+import math;
+
+string FRAMEWORK = "keras";
+
+string xcorr_root = getenv("XCORR_ROOT");
+string preprocess_rnaseq = getenv("PREPROP_RNASEQ");
+string emews_root = getenv("EMEWS_PROJECT_ROOT");
+string turbine_output = getenv("TURBINE_OUTPUT");
+
+string exp_id = argv("exp_id");
+int benchmark_timeout = toint(argv("benchmark_timeout", "-1"));
+string model_name = getenv("MODEL_NAME");
+
+printf("UQ NOISE WORKFLOW.SWIFT");
+printf("TURBINE_OUTPUT: " + turbine_output);
+
+float noise_step = 3.0; // Difference between noises
+int num_trials = 1;
+
+float x_num_noises = 4; // Number of noise levels to try
+float y_num_noises = 2; // Number of noise levels to try
+
+
+float y_noise_levels[] = [0:y_num_noises];
+int trials[]       = [0:num_trials-1];
+
+float x_noise_levels[] = [0:x_num_noises];
+
+foreach levelx, i in x_noise_levels
+{
+  foreach levely, j in y_noise_levels
+  {
+    foreach trial, k in trials
+    {
+      run_id = "%03i-%03i-%01i" % (i, j, k);
+      y_noise_level = levely * noise_step;
+      x_noise_level = levelx * noise_step;
+      result = obj("{\"x_noise_level\":%f, \"y_noise_level\":%f}" %(x_noise_level, y_noise_level), run_id);
+      printf("result %s : x_noise %0.3f y_noise %0.3f : %s", run_id, x_noise_level, y_noise_level, result);
+    }
+  }
+}
+
