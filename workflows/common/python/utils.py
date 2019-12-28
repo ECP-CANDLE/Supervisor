2e27ac3b6ab5c3b7c8443a6216673b1c733f4b50
Justin Wozniak <woz@anl.gov>
2019-12-28 16:25:10 -0600

    Fix multiple fail() functions

--- workflows/common/python/utils.py
+++ workflows/common/python/utils.py
@@ -1,13 +1,19 @@
 
 # UTILS PY
 
-def fail(e, code, message):
-    """ Fail with message due to Exception e , return exit code"""
+def fail(*args):
+    if len(args) == 1:
+        fail1(args[0])
+    elif len(args) == 3:
+        fail3(*args)
+
+def fail1(message):
+    """ Fail with message, return exit code 1 """
+    print(message)
+    exit(1)
+
+def fail3(e, code, message):
+    """ Fail with message due to Exception e , return exit code """
     print(message)
     print(str(e))
     exit(code)
-
-def fail(message):
-    """ Fail with message return exit code"""
-    print(message)
-    exit(1)
