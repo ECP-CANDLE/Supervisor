d1cadbeafc7cddba2491da31ef0cee7e739156a2
Justin Wozniak <woz@anl.gov>
2020-01-02 17:46:06 -0600

    Don't check arguments in shell script

--- workflows/cp-leaveout/scripts/workflow-stats.sh
+++ workflows/cp-leaveout/scripts/workflow-stats.sh
@@ -11,10 +11,7 @@ THIS=$( readlink --canonicalize $( dirname $0 ) )
 SUPERVISOR=$( readlink --canonicalize $THIS/../../.. )
 source $SUPERVISOR/workflows/common/sh/utils.sh
 
-SIGNATURE -H "Provide an experiment DIR (e.g., .../experiments/X042)!" \
-          DIR - ${*}
-
 export PYTHONPATH+=:$SUPERVISOR/workflows/common/python
 
 set -x
-python3 -u $THIS/workflow-stats.py $DIR
+python3 -u $THIS/workflow-stats.py ${*}
