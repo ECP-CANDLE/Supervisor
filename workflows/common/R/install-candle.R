429d22b9fe54484946aa2d43e3fb06768eeb1e93
Justin Wozniak <woz@anl.gov>
2020-04-08 16:44:09 -0500

    Improve install-candle.R

--- workflows/common/R/install-candle.R
+++ workflows/common/R/install-candle.R
@@ -4,22 +4,38 @@
 # Run this via install-candle.sh
 # Installs all R packages needed for Supervisor workflows
 
+# mlrMBO may need APT packages libxml2-dev libssl-dev curl-dev
+
+NCPUS = 16
+
 r <- getOption("repos")
 # r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
 r["CRAN"] <- "http://cran.wustl.edu/"
 options(repos = r)
 
-install.packages("RInside", Ncpus=4)
-
 # Force Plotly 4.5.6 - not latest! Do not want shiny/httpuv, it does not work on Cooley!
 install.packages("https://cran.r-project.org/src/contrib/Archive/plotly/plotly_4.5.6.tar.gz")
-install.packages("smoof", Ncpus=4)
-# mlrMBO may need APT packages libxml2-dev libssl-dev
-install.packages("mlrMBO", Ncpus=4)
-install.packages("rgenoud", Ncpus=4)
-install.packages("DiceKriging", Ncpus=4)
-install.packages("randomForest", Ncpus=4)
-install.packages("jsonlite", Ncpus=4)
-install.packages("parallelMap", Ncpus=4)
-install.packages("Rcpp", Ncpus=4)
-install.packages("RInside", Ncpus=4)
+
+PKGS=(
+    "smoof",
+    "rgenoud",
+    "DiceKriging",
+    "randomForest"
+    "jsonlite",
+    "parallelMap",
+    "RInside",
+    "mlrMBO"
+)
+
+for (pkg in PKGS) {
+  print("")
+  cat("INSTALL: ", pkg, "\n")
+  # install.packages() does not return an error status
+  install.packages(pkg, Ncpus=NCPUS, verbose=TRUE)
+  print("")
+  # Test that the pkg installed and is loadable
+  cat("LOAD:    ", pkg, "\n")
+  library(package=pkg, character.only=TRUE)
+}
+
+print("INSTALL-CANDLE: OK")
