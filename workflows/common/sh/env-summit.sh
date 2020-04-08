b230495647bd337b68ef912c6141846426afaad4
Justin Wozniak <woz@anl.gov>
2020-04-08 16:44:32 -0500

    Update for latest installation

--- workflows/common/sh/env-summit.sh
+++ workflows/common/sh/env-summit.sh
@@ -9,7 +9,7 @@ SWIFT_IMPL=py
 set +eu # modules create errors outside our control
 module load spectrum-mpi/10.3.1.2-20200121
 module unload darshan-runtime
-module load ibm-wml-ce/1.6.2-3
+# module load ibm-wml-ce/1.6.2-3
 module list
 set -eu
 
@@ -17,8 +17,12 @@ set -eu
 MED106=/gpfs/alpine/world-shared/med106
 # SWIFT=$MED106/sw/gcc-7.4.0/swift-t/2019-10-18  # Python (ibm-wml), no R
 # SWIFT=$MED106/sw/gcc-7.4.0/swift-t/2019-11-06  # Python (ibm-wml) and R
-# SWIFT=$MED106/wozniak/sw/gcc-6.4.0/swift-t/2020-03-31-c # Python (ibm-wml-ce/1.7.0-1) and R
-SWIFT=$MED106/wozniak/sw/gcc-6.4.0/swift-t/2020-04-02 # Python (ibm-wml-ce/1.6.2-3) and R
+# Python (ibm-wml-ce/1.7.0-1) and R:
+# SWIFT=$MED106/wozniak/sw/gcc-6.4.0/swift-t/2020-03-31-c
+# Python (ibm-wml-ce/1.6.2-3) and R:
+# SWIFT=$MED106/wozniak/sw/gcc-6.4.0/swift-t/2020-04-02
+# Python (med106/sw/condaenv-200408) and R:
+SWIFT=$MED106/wozniak/sw/gcc-6.4.0/swift-t/2020-04-08
 
 export TURBINE_HOME=$SWIFT/turbine
 PATH=$SWIFT/stc/bin:$PATH
@@ -26,16 +30,25 @@ PATH=$SWIFT/turbine/bin:$PATH
 
 # log_path PATH
 
-IBM_WML_CE=/autofs/nccs-svm1_sw/summit/ibm-wml-ce/anaconda-base/envs/ibm-wml-ce-1.6.2-3
+# IBM_WML_CE=/autofs/nccs-svm1_sw/summit/ibm-wml-ce/anaconda-base/envs/ibm-wml-ce-1.6.2-3
 
-export LD_LIBRARY_PATH
-LD_LIBRARY_PATH=$IBM_WML_CE/lib:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH
+# LD_LIBRARY_PATH=$IBM_WML_CE/lib:$LD_LIBRARY_PATH
 
 # Inject Python to PATH using PRELAUNCH:
 # This would be better, but is broken for ZSH users:
 # module load ibm-wml-ce/1.6.2-3
 # Must use PATH directly:
-export TURBINE_PRELAUNCH="PATH=$IBM_WML_CE/bin:\$PATH"
+# export TURBINE_PRELAUNCH="PATH=$IBM_WML_CE/bin:\$PATH"
+
+R=/gpfs/alpine/world-shared/med106/wozniak/sw/gcc-6.4.0/R-3.6.1/lib64/R
+LD_LIBRARY_PATH+=:$R/lib
+
+PY=/gpfs/alpine/world-shared/med106/sw/condaenv-200408
+LD_LIBRARY_PATH+=:$PY/lib
+export PYTHONHOME=$PY
+
+export LD_LIBRARY_PATH=/gpfs/alpine/world-shared/med106/sw/condaenv-200408/lib:$LD_LIBRARY_PATH
 
 # EMEWS Queues for R
 EQR=$MED106/wozniak/sw/gcc-6.4.0/EQ-R
