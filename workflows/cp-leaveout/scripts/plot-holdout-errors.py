9cd54af44c07f92c084000c9e3c012b837bad925
Justin Wozniak <woz@anl.gov>
2020-08-17 11:48:49 -0500

    Add header, argparse (not yet tested)

--- workflows/cp-leaveout/scripts/plot-holdout-errors.py
+++ workflows/cp-leaveout/scripts/plot-holdout-errors.py
@@ -1,6 +1,16 @@
+
+# PLOT HOLDOUT ERRORS PY
+# Plots holdout error data from distill-holdout-errors.pl
+
 import pandas
 import matplotlib.pyplot as plt
 from pandas.tools.plotting import parallel_coordinates
- 
-cpdata=pandas.read_csv('holdout-errors.parallel_plot.tsv',sep='\t', header=None, names=['Stage1','Stage2','Stage3','Stage4', 'Stage5','CLASS'])
+
+import argparse
+parser = argparse.ArgumentParser(description='Make holdout errors plot')
+parser.add_argument('input_file', help='The input errors TSV file')
+args = parser.parse_args()
+
+names = ['Stage1','Stage2','Stage3','Stage4', 'Stage5', 'CLASS']
+cpdata=pandas.read_csv(args.input_file, sep='\t', header=None, names=names)
 parallel_coordinates(cpdata, class_column="CLASS", colormap=plt.get_cmap("Set2"))
