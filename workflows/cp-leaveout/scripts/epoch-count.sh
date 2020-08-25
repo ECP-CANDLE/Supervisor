f5fe967afbc6e97b504367d14d6093f0714fca92
Justin Wozniak <woz@anl.gov>
2020-08-25 14:50:39 -0500

    Add epoch-count.sh

---
+++ workflows/cp-leaveout/scripts/epoch-count.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu
+
+# EPOCH COUNT SH
+# Report run progress in number of completed epochs
+
+THIS=$( readlink --canonicalize $( dirname $0 ) )
+CPLO=$( readlink --canonicalize $THIS/.. )
+SUPERVISOR=$( readlink --canonicalize $CPLO/../.. )
+
+source $SUPERVISOR/workflows/common/sh/utils.sh
+
+SIGNATURE -H "Provide an experiment DIR (e.g., .../experiments/X042)!" \
+          DIR - ${*}
+
+if ! [[ -d $DIR ]]
+then
+  echo "Does not exist: $DIR"
+  exit 1
+fi
+
+EXPID=$( basename $DIR )
+JOBID=$( cat $DIR/jobid.txt )
+show EXPID JOBID
+
+LOGS=( $( find $DIR -name python.log  ) )
+echo "epoch-count.sh: found ${#LOGS[@]} logs ..."
+for LOG in ${LOGS[@]}
+do
+    echo -n "$LOG :: "
+    # Pull out the last "Epoch:" line, print only the number:
+    # sed -n '/Epoch:/h;${g;s/.*Epoch: \([0-9]*\).*/\1/;p}' $LOG
+    grep "Epoch:" $LOG
+    # experiments/X362/run/1.3/save/python.log
+done # | sort -r -n -k 2 | column -t
+
