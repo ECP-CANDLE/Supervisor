1e5ec729dce9063dc46377bdb78f7faa10dccf21
Justin M Wozniak <woz@anl.gov>
2020-08-26 09:18:09 -0500

    Add a default

---
+++ workflows/common/sh/langs-app-default.sh
@@ -0,0 +1,10 @@
+
+# LANGS APP DEFAULT
+
+# Nothing by default- add settings for your system here, e.g.:
+# export PATH=...
+# export PYTHONPATH=...
+# export LD_LIBRARY_PATH=...
+
+PATH=$HOME/sfw/anaconda3/bin:$PATH
+which python
