1cb6bbd5541af5625dc46de0bfda6f13f1a9c7b4
Justin Wozniak <woz@anl.gov>
2020-12-07 11:16:41 -0600

    Note about interactive workflows

--- workflows/upf/test/cfg-sys-1.sh
+++ workflows/upf/test/cfg-sys-1.sh
@@ -1,6 +1,9 @@
 
 # UPF CFG SYS 1
 
+# Use 1 for interactive workflows
+# export INTERACTIVE=1
+
 # The number of MPI processes
 # Note that 1 process is reserved for Swift/T
 # For example, if PROCS=4 that gives you 3 workers,
@@ -23,7 +26,7 @@ export PPN=${PPN:-1}
 
 # Theta: (cf. sched-theta)
 # export QUEUE=${QUEUE:-debug-cache-quad}
-export QUEUE=${QUEUE:-debug-flat-quad}
+# export QUEUE=${QUEUE:-debug-flat-quad}
 # export PROJECT=${PROJECT:-ecp-testbed-01}
 # export PROJECT=Candle_ECP
 export PROJECT=CSC249ADOA01
@@ -31,6 +34,7 @@ export PROJECT=CSC249ADOA01
 # Summit:
 export QUEUE=${QUEUE:-batch}
 export PROJECT=med106
+# export TURBINE_LAUNCH_OPTIONS="-a1 -g6 -c7"
 
 export WALLTIME=${WALLTIME:-0:30}
 
