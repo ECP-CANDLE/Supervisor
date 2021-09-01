84a725ea1ab57888750c19593328c6e4c1f279b7
Justin Wozniak <woz@anl.gov>
2021-09-01 16:30:27 -0500

    Remove WS and bad characters

--- workflows/common/R/install-candle.R
+++ workflows/common/R/install-candle.R
@@ -19,12 +19,12 @@ options(repos = r)
 # * DONE (jsonlite)
 # 1): succeeded '/usr/local/apps/R/4.0/4.0.0/lib64/R/bin/R CMD INSTALL -l '/gpfs/gsfs9/users/BIDS-HPC/public/software/distributions/candle/dev_2/builds/R/libs' '/lscratch/64803361/Rtmpnd5yDC/downloaded_packages/jsonlite_1.7.1.tar.gz''
 # The downloaded source packages are in
-# 	‘/lscratch/64803361/Rtmpnd5yDC/downloaded_packages’
+# 	/lscratch/64803361/Rtmpnd5yDC/downloaded_packages
 # [1] ""
-# LOAD:     jsonlite 
-# Error in value[[3L]](cond) : 
-#   Package ‘jsonlite’ version 1.7.0 cannot be unloaded:
-#  Error in unloadNamespace(package) : namespace ‘jsonlite’ is imported by ‘plotly’ so cannot be unloaded
+# LOAD:     jsonlite
+# Error in value[[3L]](cond) :
+#   Package 'jsonlite' version 1.7.0 cannot be unloaded:
+#  Error in unloadNamespace(package) : namespace jsonlite is imported by plotly so cannot be unloaded
 # Calls: library ... tryCatch -> tryCatchList -> tryCatchOne -> <Anonymous>
 # Execution halted
 # ****NOTE**** that I tried installing both plotly and jsonlite the normal way (in the PKGS below instead of a specific version above) and I got the same error
