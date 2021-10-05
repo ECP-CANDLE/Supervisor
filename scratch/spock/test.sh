ec3b0ad2870f4ab34f5d5ee5837561e218c60d33
Justin Wozniak <woz@anl.gov>
2021-10-05 14:42:49 -0500

    Test Swift/T with new TF

--- scratch/spock/test.sh
+++ scratch/spock/test.sh
@@ -11,10 +11,12 @@ WORKFLOW=$1
 
 MED106=/gpfs/alpine/world-shared/med106
 ROOT=$MED106/sw/spock/gcc-10.3.0
-SWIFT=$ROOT/swift-t/2021-10-04_B
+SWIFT=$ROOT/swift-t/2021-10-05
 
 PATH=$SWIFT/stc/bin:$PATH
 PATH=$SWIFT/turbine/bin:$PATH
+
+PY=/gpfs/alpine/med106/world-shared/hsyoo/spock_tf2_py37_rocm42
 
 which swift-t
 
@@ -27,4 +29,4 @@ export PPN=2
 export TURBINE_LAUNCHER=srun
 
 set -x
-swift-t -m slurm -n $PROCS $WORKFLOW
+swift-t -m slurm -n $PROCS -e PYTHONHOME=$PY $WORKFLOW
