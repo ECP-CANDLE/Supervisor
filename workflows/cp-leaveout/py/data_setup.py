710863be3dfd7bc58281c56999515bf980f71e85
Justin Wozniak <woz@anl.gov>
2021-10-18 11:38:01 -0500

    Write partial training file to temp file, then rename

--- workflows/cp-leaveout/py/data_setup.py
+++ workflows/cp-leaveout/py/data_setup.py
@@ -1,8 +1,7 @@
 
 # DATA SETUP PY
 
-import json
-import os, sys
+import os, sys, time
 
 from pathlib import Path
 import traceback
@@ -20,39 +19,45 @@ class TopN_Args:
         self.drug_feature_selection = None
         self.output = output
 
+def setup_nvm(params):
+    # username = os.environ['USER']  # No longer works on Summit 2021-10-13
+    username = params["user"]
+    nvme_enabled = Path('/mnt/bb/{}'.format(username)).exists()
+    # nvme_enabled = True
+    print("NVMe: %r" % nvme_enabled)
+    if not nvme_enabled:
+        return
+    # copy original datafrom to NVMe disk space
+    try:
+        src = Path(params["dataframe_from"])
+        dest = Path("/mnt/bb/{}/{}".format(username, src.name))
+        if not dest.exists():
+            start = time.time()
+            count = dest.write_bytes(src.read_bytes())
+            stop = time.time()
+            duration = stop - start
+            rate = count / duration / (1024*1024)
+            print("File copy completed. Original dataframe " +
+                  "copied to NVM in %0.1f seconds (%0.1f MB/s)." %
+                  (duration, rate))
+        else:
+            print("File copy skipped. " +
+                  "Original dataframe already exists in NVM.")
+    except Exception as e:
+        print("Error occurred in copying original dataframe\n" +
+              str(e))
+        traceback.print_exc()
+        return ModelResult.ERROR
+    params["dataframe_from"] = dest.resolve()
+    # Do not do this: it changes the location of the training data
+    # params["use_exported_data"] = "/mnt/bb/{}/{}".format(username, params["use_exported_data"])
+    return params
+
+
 def pre_run(params):
     import sys, time
     print("data_setup.pre_run(): node: '%s' ..." % params["node"])
     sys.stdout.flush()
-
-    # check NVMe disk is available
-    # username = os.environ['USER']
-    # nvme_enabled = Path('/mnt/bb/{}'.format(username)).exists()
-    nvme_enabled = False
-    if nvme_enabled:
-        # copy original datafrom to NVMe disk space
-        try:
-            src = Path(params["dataframe_from"])
-            dest = Path("/mnt/bb/{}/{}".format(username, src.name))
-            if not dest.exists():
-                start = time.time()
-                count = dest.write_bytes(src.read_bytes())
-                stop = time.time()
-                duration = stop - start
-                rate = count / duration / (1024*1024)
-                print("File copy completed. Original dataframe " +
-                      "copied to NVM in %0.1f seconds (%0.1f MB/s)." %
-                      (duration, rate))
-            else:
-                print("File copy skipped. " +
-                      "Original dataframe already exists in NVM.")
-        except Exception as e:
-            print("Error occurred in copying original dataframe\n" +
-                  str(e))
-            traceback.print_exc()
-            return ModelResult.ERROR
-        params["dataframe_from"] = dest.resolve()
-        params["use_exported_data"] = "/mnt/bb/{}/{}".format(username, params["use_exported_data"])
 
     # softlink to cache & config file
     # build node specific training/validation dataset
@@ -64,7 +69,7 @@ def pre_run(params):
 
     data = params["benchmark_data"]
     try:
-        for filename in [ "uno_auc_model.txt" ]: # "cache",
+        for filename in [ "uno_auc_model.txt" ]:  # "cache",
             if not os.path.islink(filename):
                 src = f"{data}/{filename}"
                 print("data_setup: src:  (%s)" % src)
@@ -83,7 +88,9 @@ def pre_run(params):
               args.output)
         sys.stdout.flush()
         if not os.path.exists(args.output):
-            sys.stdout.flush()
+            params = setup_nvm(params)
+            out_orig = args.output
+            args.output = out_orig + ".part"
             start = time.time()
             topN_to_uno.build_dataframe(args)
             stop = time.time()
@@ -91,6 +98,10 @@ def pre_run(params):
             print("data_setup: build_dataframe() OK : " +
                   "%0.1f seconds." % duration)
             sys.stdout.flush()
+            os.rename(args.output, out_orig)
+            print("data_setup: rename() OK")
+            sys.stdout.flush()
+            args.output = out_orig
         else:
             print("data_setup: dataframe exists: %s" %
                   os.path.realpath(args.output))
