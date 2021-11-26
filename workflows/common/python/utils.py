756e59041e085b353013773cf91166e6bf432e08
Justin Wozniak <woz@anl.gov>
2021-11-25 18:58:50 -0600

    flake8 fixes

--- workflows/common/python/utils.py
+++ workflows/common/python/utils.py
@@ -1,16 +1,21 @@
 
 # UTILS PY
 
+import os
+
+
 def fail(*args):
     if len(args) == 1:
         fail1(args[0])
     elif len(args) == 3:
         fail3(*args)
 
+
 def fail1(message):
     """ Fail with message, return exit code 1 """
     print(message)
     exit(1)
+
 
 def fail3(e, code, message):
     """ Fail with message due to Exception e , return exit code """
@@ -18,11 +23,13 @@ def fail3(e, code, message):
     print(str(e))
     exit(code)
 
+
 def avg(values):
     total = 0.0
     for v in values:
         total += v
     return total / len(values)
+
 
 def append(filename, text):
     try:
