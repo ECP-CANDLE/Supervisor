04a368e5062a776e8a6c6682580e63cab5c315d2
Rajeev Jain <rajeeja@gmail.com>
2022-04-15 11:52:15 -0400

    o Update langs-app for summit

--- workflows/common/sh/langs-app-summit.sh
+++ workflows/common/sh/langs-app-summit.sh
@@ -1,17 +1,13 @@
 # LANGS APP SUMMIT SH
 
-# WIP 2019-02-28
-
 APP_PYTHONPATH=${APP_PYTHONPATH:-$PYTHONPATH}
 
 # Clear anything set by the system or Swift/T environment
 unset PYTHONPATH
 unset LD_LIBRARY_PATH
 
-# ROOT=/ccs/proj/med106/gounley1/summit
-ROOT=/ccs/proj/med106/hsyoo/summit
-export PY=$ROOT/conda36
-export LD_LIBRARY_PATH=/sw/summit/cuda/10.1.168/lib64:/sw/summit/gcc/4.8.5/lib64:$PY/lib
-export PYTHONHOME=$ROOT/conda36
+export PY=/gpfs/alpine/world-shared/med106/sw/open-ce-1.1.3-py37/
+export LD_LIBRARY_PATH=$PY/lib:$LD_LIBRARY_PATH
+export PYTHONHOME=$PY
 export PATH=$PYTHONHOME/bin:$PATH
-export PYTHONPATH=$PYTHONHOME/lib/python3.6:$PYTHONHOME/lib/python3.6/site-packages:$APP_PYTHONPATH
+export PYTHONPATH=$PYTHONHOME/lib/python3.9:$PYTHONHOME/lib/python3.9/site-packages:$APP_PYTHONPATH
