0e22e6292d0dcd3770a60598c8bbed1102baa86f
Justin Wozniak <woz@anl.gov>
2022-04-15 12:35:09 -0500

    Fix unset variables in langs-app-summit

--- workflows/common/sh/langs-app-summit.sh
+++ workflows/common/sh/langs-app-summit.sh
@@ -1,13 +1,12 @@
+
 # LANGS APP SUMMIT SH
 
-APP_PYTHONPATH=${APP_PYTHONPATH:-$PYTHONPATH}
+# Allow for user PYTHONPATH additions:
+APP_PYTHONPATH=${APP_PYTHONPATH:-}
 
-# Clear anything set by the system or Swift/T environment
-unset PYTHONPATH
-unset LD_LIBRARY_PATH
-
-export PY=/gpfs/alpine/world-shared/med106/sw/open-ce-1.1.3-py37/
-export LD_LIBRARY_PATH=$PY/lib:$LD_LIBRARY_PATH
+# Overwrite anything else set by the system or Swift/T environment:
+export PY=/gpfs/alpine/world-shared/med106/sw/open-ce-1.1.3-py37
+export LD_LIBRARY_PATH=$PY/lib
 export PYTHONHOME=$PY
 export PATH=$PYTHONHOME/bin:$PATH
 export PYTHONPATH=$PYTHONHOME/lib/python3.9:$PYTHONHOME/lib/python3.9/site-packages:$APP_PYTHONPATH
