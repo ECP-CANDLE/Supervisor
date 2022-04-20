7537b03221cb458187d48e21527ab9a450e96e5d
Justin Wozniak <woz@anl.gov>
2022-04-20 14:19:32 -0500

    Check for error in input name translation

--- workflows/cp-leaveout/scripts/shrink-output-single.sh
+++ workflows/cp-leaveout/scripts/shrink-output-single.sh
@@ -9,6 +9,12 @@ OUTPUT=$2
 
 T=${INPUT/out/tr}
 
+if [ $INPUT == $T ]
+then
+  echo "shrink-output-single.sh: ERROR: INPUT is wrong."
+  exit 1
+fi
+
 tr "\r" "\n" < $INPUT > $T
 python $THIS/shrink-output.py $T $OUTPUT
 
