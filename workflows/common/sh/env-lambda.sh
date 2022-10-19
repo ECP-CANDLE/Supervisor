d8236d4e6256f85b3f77c524d565a0b21ca67d38
Justin Wozniak <woz@anl.gov>
2022-10-19 13:59:32 -0500

    Initial env file for Lambda- seems to work

---
+++ workflows/common/sh/env-lambda.sh
@@ -0,0 +1,27 @@
+
+# ENV Lambda
+# Environment settings for Lambda (Swift, Python, R, Tcl, etc.)
+
+# Everything is installed in here:
+SFW=/home/woz/Public/sfw
+
+SWIFT=$SFW/swift-t/2022-10-19
+PY=$SFW/Anaconda
+EQR=$SFW/EQ-R
+R=$SFW/R-4.1.0
+
+PATH=$SWIFT/stc/bin:$PATH
+PATH=$PY/bin:$PATH
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:$R/lib/R/lib
+
+# How to run CANDLE models:
+SWIFT_IMPL="app"
+
+# Log settings to output
+echo "Programs:"
+which python swift-t | nl
+# Cf. utils.sh
+show     PYTHONHOME
+log_path LD_LIBRARY_PATH
+log_path PYTHONPATH
