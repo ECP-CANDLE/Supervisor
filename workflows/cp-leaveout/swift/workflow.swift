42fce3ddc33c21bc28f0b5b203db3fbe9609c5ca
Justin Wozniak <woz@anl.gov>
2023-02-26 23:41:33 -0600

    Support multi-digit node components

--- workflows/cp-leaveout/swift/workflow.swift
+++ workflows/cp-leaveout/swift/workflow.swift
@@ -6,6 +6,7 @@
   Or specify the N, S values:
   'swift-t workflow.swift -N=4 -S=6 <FILE SETTINGS>'
   for ### tasks.
+
   Flags:
   -N : Number of nodes per stage (see default in code)
   -S : Number of stages          (see default in code)
@@ -22,6 +23,8 @@
   --benchmark_data=<DIR> : Used by data_setup to set softlinks to
                            Uno cache and uno_auc_model.txt
 
+  NOTE: "token" variables are used to ensure that children do
+        not run before their parents
   NOTE: This workflow has some complex Python Exception handling
         code that will be pushed into Swift/T for conciseness...
   NOTE: On Summit, you have to use sys.stdout.flush() after
@@ -57,7 +60,6 @@ else
   N = 0;
 }
 // Maximum stage number with default
-// (tested up to S=7, 21,844 dummy tasks)
 int S;
 S_s = argv("S", "2");
 assert(strlen(S_s) > 0, "Set argument S with -S=<S>") =>
@@ -92,35 +94,49 @@ string turbine_output = getenv("TURBINE_OUTPUT");
 printf("plangen: runtype:" + runtype);
 printf("benchmark_data: " + benchmark_data);
 
-// // For compatibility with obj():
+// For compatibility with obj():
 global const string FRAMEWORK = "keras";
 
-/** RUN STAGE: A recursive function that manages the stage dependencies */
+/**
+   RUN STAGE: A recursive function that manages the stage dependencies
+   token_parent: Blocks progress until the parent node is done
+   parent:       The parent node,  e.g., "1.2.3"
+   this:         The current node, e.g., "1.2.3.4"
+*/
 (void v)
-run_stage(int N, int S, string this, int stage, void block,
-          string plan_id, string db_file, string runtype)
+run_stage(string db_file, string plan_id, string runtype,
+          void token_parent, int stage, string parent, string this)
 {
-  // printf("stage: %i this: %s", stage, this);
+  printf("stage: %i parent: %s this: %s", stage, parent, this);
   // Run the model
-  void parent = run_single(this, stage, block, plan_id);
+  void token_this = run_single(plan_id, token_parent, stage,
+                               parent, this);
 
   if (stage < S)
   {
     // Recurse to the child stages
-    foreach id_child in [1:N]
+    foreach child in [1:N]
     {
-      run_stage(N, S,
-                // We want padded node IDs like "1.01.03" , "1.10.16"
-                "%s.%02i" % (this, id_child),
-                stage+1, parent,
-                plan_id, db_file, runtype);
+      run_stage(db_file, plan_id, runtype,
+                token_this, stage+1,
+                this,
+                "%s.%i" % (this, child)
+                ); // N, S,
     }
   }
   v = propagate();
 }
 
-/** RUN SINGLE: Set up and run a single model via obj(), plus the SQL ops */
-(void v) run_single(string node, int stage, void block, string plan_id)
+/**
+   RUN SINGLE: Set up and run a single model via obj(), plus the SQL ops
+   token: Block on token so that this node does not run until the
+   parent is complete.
+   stage:  The current stage, e.g., 4
+   parent: The parent node,   e.g., "1.2.3"
+   this:   The current node,  e.g., "1.2.3.4"
+*/
+(void v) run_single(string plan_id, void token, int stage,
+                    string parent, string this)
 {
   if (stage == 0)
   {
@@ -128,24 +144,24 @@ run_stage(int N, int S, string this, int stage, void block,
   }
   else
   {
-    json_fragment = make_json_fragment(node, stage);
-    json = "{\"node\": \"%s\", %s}" % (node, json_fragment);
-    block =>
-      printf("run_single(): running obj(%s)", node) =>
+    json_fragment = make_json_fragment(parent, this, stage);
+    json = "{\"node\": \"%s\", %s}" % (this, json_fragment);
+    token =>
+      printf("run_single(): running obj(%s)", this) =>
       // Insert the model run into the DB
-      result1 = plangen_start(node, plan_id);
+      result1 = plangen_start(this, plan_id);
     assert(result1 != "EXCEPTION", "Exception in plangen_start()!");
     if (result1 == "0")
     {
       // Run the model
-      obj_result = obj(json, exp_id, node);
+      obj_result = obj(json, exp_id, this);
       printf("run_single(): completed: node: '%s' result: '%s'",
-             node, obj_result);
+             this, obj_result);
       // Update the DB to complete the model run
       string result2;
       if (obj_result != "RUN_EXCEPTION")
       {
-        result2 = plangen_stop(node, plan_id);
+        result2 = plangen_stop(this, plan_id);
       }
       else
       {
@@ -160,37 +176,35 @@ run_stage(int N, int S, string this, int stage, void block,
     else // result1 != 0
     {
       printf("run_single(): plan node already marked complete: " +
-             "%s result=%s", node, result1) =>
+             "%s result=%s", this, result1) =>
         v = propagate();
     }
   }
 }
 
 /** MAKE JSON FRAGMENT: Construct the JSON parameter fragment for the model */
-(string result) make_json_fragment(string this, int stage)
+(string result) make_json_fragment(string parent, string this, int stage)
 {
   int epochs = compute_epochs(stage);
   json_fragment = ----
-"pre_module":     "data_setup",
-"post_module":    "data_setup",
-"plan":           "%s",
-"config_file":    "uno_auc_model.txt",
-"cache":          "cache/top6_auc",
-"user":           "%s",
-"dataframe_from": "%s",
-"save_weights":   "save/model.h5",
-"gpus": "0",
-"epochs": %i,
-"es": "True",
-"early_stopping": %i,
+"pre_module":        "data_setup",
+"post_module":       "data_setup",
+"plan":              "%s",
+"config_file":       "uno_auc_model.txt",
+"cache":             "cache/top6_auc",
+"user":              "%s",
+"dataframe_from":    "%s",
+"save_weights":      "save/model.h5",
+"gpus":              "0",
+"epochs":            %i,
+"es":                "True",
+"early_stopping":    %i,
 "use_exported_data": "topN.uno.h5",
-"benchmark_data": "%s"
+"benchmark_data":    "%s"
 ---- %
 (plan_json, user, dataframe_csv, epochs, early_stopping, benchmark_data);
   if (stage > 1)
   {
-    n = strlen(this);
-    parent = substring(this, 0, n-3);
     result = json_fragment + ----
 ,
 "initial_weights": "../%s/save/model.h5"
@@ -217,5 +231,5 @@ assert(plan_id != "-1", "Plan already exists!");
 
 // Kickoff the workflow
 stage = 0;
-run_stage(N, S, "1", stage, propagate(), plan_id, db_file, runtype);
-// printf("CP LEAVEOUT WORKFLOW: RESULTS: COMPLETE");
+run_stage(db_file, plan_id, runtype,
+          propagate(), stage, "", "1");
