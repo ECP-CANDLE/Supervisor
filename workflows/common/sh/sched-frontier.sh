2e0c41061eeb605936f732dea675562156ac57fb
Justin Wozniak <woz@anl.gov>
2023-03-01 17:18:42 -0600

    Respect user TURBINE_PRELAUNCH

--- workflows/common/sh/sched-frontier.sh
+++ workflows/common/sh/sched-frontier.sh
@@ -9,6 +9,7 @@ MACHINE="-m slurm"
 #export QUEUE=${QUEUE:-batch}
 export PROJECT=${PROJECT:-MED106}
 
-export TURBINE_PRELAUNCH="source activate /gpfs/alpine/med106/proj-shared/hm0/candle_tf_2.10"
+PY=/gpfs/alpine/med106/proj-shared/hm0/candle_tf_2.10
+export TURBINE_PRELAUNCH="source activate $PY ; ${TURBINE_PRELAUNCH:-}"
 
 export TURBINE_LAUNCH_OPTIONS="--gpus-per-task=1 --gpus-per-node=$PPN"
