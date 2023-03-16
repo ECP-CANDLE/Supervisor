f0e4eaef5d3bf944db0ca30042b79a82a14a68bc
Justin Wozniak <woz@anl.gov>
2023-03-16 15:20:24 -0500

    WIP cmp-cv workflow

---
+++ workflows/cmp-cv/py/compare.py
@@ -0,0 +1,21 @@
+
+CANDLE_DATA_DIR = os.getenv("CANDLE_DATA_DIR")
+
+def compare(exp_id, run_id):
+
+    print(f"compare: run_id={run_id}")
+    gParams = read_params(exp_id, run_id)
+    model = gParams("model_name")
+
+    directory = f"{CANDLE_DATA_DIR}/Output/{exp_id}/{run_id}"
+    df_res = pd.read_csv(f"{directory}/test_predictions.csv")
+
+    # a class to calculate errors for subsets of the validation/test set
+    bmk = Benchmark(fp_path='drug_features.csv') # TODO: have to have a drug features for a common test set
+    subset_err = bmk.error_by_feature_domains_model(df_res, conditions)
+
+    # collect results for comparison
+    subset_err.set_index('prop', inplace=True) # TODO: use 'prop' as a parameter and move it to cmp_models.txt
+    cmp_results[run_id] = subset_err.loc[cmp_prop, 'error'] # this is the property based on which we want to do the comparison
+    with open(f"{directory}/subset_err.txt", "w") as fp:
+        fp.write(str(cmp_results[run_id]))
--- workflows/cmp-cv/swift/workflow.swift
+++ workflows/cmp-cv/swift/workflow.swift
@@ -11,38 +11,59 @@ import string;
 import sys;
 
 import candle_utils;
-report_env();
+// report_env();
 
 string FRAMEWORK = "keras";
 
 // Scan command line
-file   plan = input(argv("plan"));
+// file   plan = input(argv("plan"));
+file   model_file = input(argv("models"));
+file   gparams_file = input(argv("gparams"));
 int    benchmark_timeout = string2int(argv("benchmark_timeout", "-1"));
 
-string model_name     = getenv("MODEL_NAME");
 string expid          = getenv("EXPID");
 string turbine_output = getenv("TURBINE_OUTPUT");
 
 // Report some key facts:
-printf("CMP-CV: %s", filename(plan));
+printf("CMP-CV: %s", filename(model_file));
 system1("date \"WORKFLOW START: +%Y-%m-%d %H:%M\"");
 
 // Read unrolled parameter file
-string plan_lines[] = file_lines(plan);
+// string plan_lines[] = file_lines(plan);
+string model_lines[] = file_lines(model_file);
+
+string gparams_lines[] = file_lines(gparams_file);
 
 // Resultant output values:
 string results[];
+// string run_ids[];
 
-// Evaluate each parameter set
-foreach params, i in plan_lines
+compare(string exp_id, string run_id)
 {
-  printf("params: %s", params);
-  runid = json_get(params, "id");
-  results[i] = obj(params, expid, runid);
-  assert(results[i] != "EXCEPTION", "exception in obj()!");
+  python_persist("import compare",
+                 "compare.compare("%s", \"%s\")") % (exp_id, run_id));
 }
 
-// Join all result values into one big semicolon-delimited string
-string result = join(results, ";");
-// and print it
-printf("WORKFLOW RESULT: " + result);
+// Evaluate each parameter set
+foreach model, i in model_lines
+{
+  foreach gparam, j in gparams_lines
+  {
+    run_id = i*1000000 + j;
+
+    // printf("model: %s", model);
+    m = "\"model_name\": \"%s\"" % model;
+
+    gparams = replace(gparam, "MORE_PARAMS", m, 0);
+    printf(gparams);
+    results[run_id] = obj(params, expid, runid) =>
+      compare(exp_id, run_id);
+
+    // assert(results[i] != "EXCEPTION", "exception in obj()!");
+  }
+}
+
+// // Join all result values into one big semicolon-delimited string
+// string result = join(run_ids, ";");
+// // and print it
+// printf("WORKFLOW RESULT: " + result);
