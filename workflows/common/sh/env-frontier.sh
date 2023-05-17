e6182010fbe04646e23a86c4b7aada2701ac4c9f
Justin Wozniak <woz@anl.gov>
2023-05-17 10:57:41 -0500

    Try new Swift/T

--- workflows/common/sh/env-frontier.sh
+++ workflows/common/sh/env-frontier.sh
@@ -8,7 +8,8 @@ CANDLE_MODEL_IMPL=py
 # SWIFT=$ROOT/swift-t/2023-04-26 # Good
 
 ROOT=/lustre/orion/med106/world-shared/sfw
-SWIFT=$ROOT/swift-t/2023-05-08 # MPI-IO fix
+# SWIFT=$ROOT/swift-t/2023-05-08 # MPI-IO fix
+SWIFT=$ROOT/swift-t/2023-05-10  # PMI SYNC
 
 export TURBINE_HOME=$SWIFT/turbine
 PATH=$SWIFT/stc/bin:$PATH
