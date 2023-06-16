448cf3cfec40c36380dbb1ea27954e4e6e9016c6
Justin Wozniak <woz@anl.gov>
2023-06-16 16:16:27 -0500

    Add labels

--- workflows/dense-noise/scripts/mk-plot.py
+++ workflows/dense-noise/scripts/mk-plot.py
@@ -18,9 +18,11 @@ df = pd.read_hdf(args.data, key="plot")
 
 import seaborn as sns
 
-plot = sns.heatmap(df, cmap="viridis")
+plot = sns.heatmap(df, # cmap="viridis")
+                   cmap=sns.diverging_palette(220, 20, as_cmap=True))
 plot.set_xlabel("layer size")
-plot.set_ylabel("noise level")
-plot.collections[0].colorbar.set_label("error")
+plot.set_ylabel("noise level (%)")
+plot.collections[0].colorbar.set_label("error (val_loss)")
 fig = plot.get_figure()
 fig.savefig(args.plot)
+print("plotted: " + args.plot)
