158dec84d8dcffcc20f3df421cf8ca65974cd9e1
Justin Wozniak <woz@anl.gov>
2023-06-19 16:46:18 -0500

    New show-h5

---
+++ workflows/dense-noise/scripts/show-h5.py
@@ -0,0 +1,20 @@
+
+# SHOW H5 PY
+
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("data",
+                    help="The extracted data " +
+                         "(output from ./plot-extract.py)")
+parser.add_argument("key",
+                    help="The H5 key to show")
+
+args = parser.parse_args(sys.argv[1:])
+
+import pandas as pd
+
+df = pd.read_hdf(args.data, key=args.key)
+print(str(df))
