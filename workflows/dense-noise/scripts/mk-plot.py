9a517460c3856d48e9ec4ffc75711a072473978b
Justin Wozniak <woz@anl.gov>
2023-06-19 14:10:57 -0500

    Minor improvements

--- workflows/dense-noise/scripts/mk-plot.py
+++ workflows/dense-noise/scripts/mk-plot.py
@@ -15,14 +15,20 @@ args = parser.parse_args(sys.argv[1:])
 import pandas as pd
 
 df = pd.read_hdf(args.data, key="plot")
+df = -df
+m = df.median().median()
 
 import seaborn as sns
 
 plot = sns.heatmap(df, # cmap="viridis")
+                   center=m,
                    cmap=sns.diverging_palette(220, 20, as_cmap=True))
+
+# current_values = plot.gca().get_yticks()
+# plot.gca().set_yticklabels(['{:,.0f}'.format(x) for x in current_values])
+
 plot.set_xlabel("layer size")
 plot.set_ylabel("noise level (%)")
-# plot.collections[0].colorbar.set_label("error (val_loss)")
 plot.collections[0].colorbar.set_label("error diff (val_loss)")
 fig = plot.get_figure()
 fig.savefig(args.plot)
