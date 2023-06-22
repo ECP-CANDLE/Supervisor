c7ed20749554ab60824b9b9e8956b1c4798d3611
Justin Wozniak <woz@anl.gov>
2023-06-22 16:59:07 -0500

    New Swift/T location

--- workflows/common/sh/env-frontier.sh
+++ workflows/common/sh/env-frontier.sh
@@ -4,12 +4,10 @@
 # CANDLE_MODEL_IMPL=echo
 CANDLE_MODEL_IMPL=py
 
-# ROOT=/autofs/nccs-svm1_home1/wozniak/Public/sfw/frontier
-# SWIFT=$ROOT/swift-t/2023-04-26 # Good
-
 ROOT=/lustre/orion/med106/world-shared/sfw
-# SWIFT=$ROOT/swift-t/2023-05-08 # MPI-IO fix
-SWIFT=$ROOT/swift-t/2023-05-10  # PMI SYNC
+# SWIFT=$ROOT/swift-t/2023-05-08  # MPI-IO fix
+# SWIFT=$ROOT/swift-t/2023-05-10  # PMI SYNC
+SWIFT=$ROOT/swift-t/2023-06-22    # srun usage update
 
 export TURBINE_HOME=$SWIFT/turbine
 PATH=$SWIFT/stc/bin:$PATH
