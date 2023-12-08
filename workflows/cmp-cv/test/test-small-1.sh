dfdfa922528bd9c13acc57221b5b7bcf237bfc9f
Justin Wozniak <woz@anl.gov>
2023-12-08 15:16:53 -0600

    Use new supervisor tool model

--- workflows/cmp-cv/test/test-small-1.sh
+++ workflows/cmp-cv/test/test-small-1.sh
@@ -16,6 +16,7 @@ SITE=$1
 THIS=$(               cd $( dirname $0 ) ; /bin/pwd )
 EMEWS_PROJECT_ROOT=$( cd $THIS/..        ; /bin/pwd )
 WORKFLOWS_ROOT=$(     cd $THIS/../..     ; /bin/pwd )
+SUPERVISOR_HOME=$(    cd $THIS/../../..  ; /bin/pwd )
 export EMEWS_PROJECT_ROOT
 
 export MODEL_RETURN="val_loss"
@@ -24,5 +25,8 @@ CFG_SYS=$THIS/cfg-sys-1.sh
 # export MODEL_NAME="DrugCell"
 # export CANDLE_IMAGE=/homes/ac.gpanapitiya/ccmg-mtg/Singularity/DrugCell.sif
 export CANDLE_MODEL_TYPE="SINGULARITY"
+
+source $SUPERVISOR_HOME/workflows/common/sh/utils.sh
+sv_path_append $SUPERVISOR_HOME/workflows/common/sh
 
 $EMEWS_PROJECT_ROOT/swift/workflow.sh $SITE -a $CFG_SYS $THIS/upf-1.txt $THIS/models-1.txt
