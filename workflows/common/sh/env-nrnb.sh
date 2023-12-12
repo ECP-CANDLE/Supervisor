da3bc6106062203878ffa942798272b01ac04651
Justin M Wozniak <jmwozniak@nrnb-login-01.cm.cluster>
2023-12-12 14:05:28 -0800

    Turn on Singularity mode

--- workflows/common/sh/env-nrnb.sh
+++ workflows/common/sh/env-nrnb.sh
@@ -4,5 +4,6 @@
 SWIFT=/cellar/users/jmwozniak/Public/sfw/swift-t/2023-12-11
 
 PATH=$SWIFT/stc/bin:$PATH
+PATH=/cm/local/apps/singularity/current/bin:$PATH
 
-export CANDLE_MODEL_IMPL=app
+export CANDLE_MODEL_IMPL="container"
