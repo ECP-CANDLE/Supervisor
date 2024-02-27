54d18f34ce122bbc4a5d9c6c1ff6cade233cfe03
Justin Wozniak <woz@anl.gov>
2024-02-27 14:41:01 -0600

    New HPO Table

---
+++ scripts/hpo_table.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""
+HPO TABLE
+
+Extract a CSV table from HPO results model.logs
+"""
+
+
+hyperparameters = [ "learning_rate", "dropout" ]
+hyperparameters.sort()
+
+
+def main():
+    args = parse_args()
+    model_runs = find_runs(args.experiment_directory)
+    # A list of dict.  The dict contains the stats for the run
+    table = []
+    for run in model_runs:
+        add_stats(run, table)
+    write_table(table, args.output_csv)
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(prog="HPO Table")
+    parser.add_argument("experiment_directory")
+    parser.add_argument("output_csv")
+    args = parser.parse_args()
+    return args
+
+
+def find_runs(experiment_directory):
+    import glob
+    L = glob.glob(experiment_directory + "/run*")
+    return L
+
+
+def add_stats(run, table):
+    values = {}
+    model_log = run + "/model.log"
+    with open(model_log, "r") as fp:
+        while True:
+            line = fp.readline()
+            # Try until we find something:
+            if len(line) == 0: break
+            tokens = line.split()
+            if len(tokens) < 2: continue
+            prefix = tokens[0]
+            if prefix == "IMPROVE_RESULT":
+                values["metric"] = tokens[1][:-1]
+                values["result"] = tokens[2]
+                continue
+            for hp in hyperparameters:
+                if prefix == hp:
+                    values[hp] = tokens[1]
+                    break
+            if len(tokens) < 4: continue
+            if tokens[3] == "RUNID:":
+                values["run_id"] = tokens[4][4:]
+                continue
+    table.append(values)
+
+
+def write_table(table, output_csv):
+    import csv
+    # Create the header
+    row = []
+    row += ["run"]
+    row += hyperparameters
+    row += ["time", "metric", "result"]
+    with open(output_csv, "w") as fp:
+        writer = csv.writer(fp, delimiter=",")
+        # Write the header
+        writer.writerow(row)
+        for values in table:
+            row.clear()
+            row.append(values["run_id"])
+            for hp in hyperparameters:
+                row.append(values[hp])
+            row.append(0)
+            row.append(values["metric"])
+            row.append(values["result"])
+            writer.writerow(row)
+
+
+main()
