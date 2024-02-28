42954a13ff7fa7b2eba5197a41cd10173d7e0be5
Justin Wozniak <woz@anl.gov>
2024-02-28 16:12:02 -0600

    HPO for LGBM works

--- workflows/GA/test/cfg-lgbm-1.sh
+++ workflows/GA/test/cfg-lgbm-1.sh
@@ -4,7 +4,7 @@
 export CANDLE_MODEL_TYPE="SINGULARITY"
 export MODEL_NAME=/software/improve/images/lgbm_0.6.0.sif  #Lambda
 
-export PARAM_SET_FILE=random_param_space.json
+export PARAM_SET_FILE=lgbm_param_space-1.json
 
 export FRAMEWORK="keras"
 export PROCS=4
