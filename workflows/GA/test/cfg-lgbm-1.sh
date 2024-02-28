5096316981444a2d7aea565faa96d27441bdac7c
Justin Wozniak <woz@anl.gov>
2024-02-28 14:30:38 -0600

    New HPO test for LGBM

---
+++ workflows/GA/test/cfg-lgbm-1.sh
@@ -0,0 +1,10 @@
+
+# CFG LGBM TEST
+
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/software/improve/images/lgbm_0.6.0.sif  #Lambda
+
+export PARAM_SET_FILE=random_param_space.json
+
+export FRAMEWORK="keras"
+export PROCS=4
