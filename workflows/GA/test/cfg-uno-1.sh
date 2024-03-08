86dcd1aa371e2f7c8dbf7d728aec4105a259b5da
Justin Wozniak <woz@anl.gov>
2024-03-08 14:48:55 -0600

    Add more sizes

--- workflows/GA/test/cfg-uno-1.sh
+++ workflows/GA/test/cfg-uno-1.sh
@@ -1,8 +1,23 @@
 
 # CFG UNO TEST
 
-export MODEL_NAME=uno
-export PARAM_SET_FILE=random_param_space.json
+export CANDLE_MODEL_TYPE="SINGULARITY"
+export MODEL_NAME=/software/improve/images/Uno_Improve.sif  #Lambda
+export PARAM_SET_FILE=uno_param_space-1.json
 
 export FRAMEWORK="keras"
+
+# # SMALL:
+# export PROCS=4
+# export POPULATION_SIZE=4
+# export NUM_ITERATIONS=3
+
+# # MEDIUM:
 export PROCS=4
+export POPULATION_SIZE=16
+export NUM_ITERATIONS=4
+
+# # LARGE:
+# export PROCS=12
+# export POPULATION_SIZE=80
+# export NUM_ITERATIONS=10
