e8f66edddde9ece98e60033ba743d28c0669d4a1
Justin Wozniak <woz@anl.gov>
2024-03-08 14:35:02 -0600

    Add more sizes

--- workflows/GA/test/cfg-lgbm-1.sh
+++ workflows/GA/test/cfg-lgbm-1.sh
@@ -7,4 +7,18 @@ export MODEL_NAME=/software/improve/images/lgbm_0.6.0.sif  #Lambda
 export PARAM_SET_FILE=lgbm_param_space-1.json
 
 export FRAMEWORK="keras"
+
+# # SMALL:
+# export PROCS=6
+# export POPULATION_SIZE=4
+# export NUM_ITERATIONS=3
+
+# # MEDIUM:
+# export PROCS=10
+# export POPULATION_SIZE=16
+# export NUM_ITERATIONS=4
+
+# LARGE:
 export PROCS=4
+export POPULATION_SIZE=80
+export NUM_ITERATIONS=10
