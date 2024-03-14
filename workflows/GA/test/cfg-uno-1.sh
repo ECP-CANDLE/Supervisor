7a62870cd81596f09a77df9add91fe4f08bea0f8
Justin Wozniak <woz@anl.gov>
2024-03-14 13:06:05 -0500

    Start Uno/Polaris

--- workflows/GA/test/cfg-uno-1.sh
+++ workflows/GA/test/cfg-uno-1.sh
@@ -1,21 +1,27 @@
 
 # CFG UNO TEST
 
-export CANDLE_MODEL_TYPE="SINGULARITY"
-export MODEL_NAME=/software/improve/images/Uno_Improve.sif  #Lambda
+# # For containers:
+# export CANDLE_MODEL_TYPE="SINGULARITY"
+# export MODEL_NAME=/software/improve/images/Uno_Improve.sif  #Lambda
+
+# For plain Python:
+export MODEL_NAME=uno
+export MODEL_PYTHON_DIR=$HOME/proj/IMPROVE-Benchmarks/Pilot1/Uno
+
 export PARAM_SET_FILE=uno_param_space-1.json
 
 export FRAMEWORK="keras"
 
-# # SMALL:
-# export PROCS=4
-# export POPULATION_SIZE=4
-# export NUM_ITERATIONS=3
+# SMALL:
+export PROCS=4
+export POPULATION_SIZE=4
+export NUM_ITERATIONS=3
 
 # # MEDIUM:
-export PROCS=4
-export POPULATION_SIZE=16
-export NUM_ITERATIONS=4
+# export PROCS=4
+# export POPULATION_SIZE=16
+# export NUM_ITERATIONS=4
 
 # # LARGE:
 # export PROCS=12
