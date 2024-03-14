69229198828eb3dd530feda7cf5f474810c50d71
Justin Wozniak <woz@anl.gov>
2024-03-14 11:51:31 -0500

    Update Polaris settings

--- workflows/common/sh/env-polaris.sh
+++ workflows/common/sh/env-polaris.sh
@@ -4,10 +4,14 @@
 # CANDLE_MODEL_IMPL=echo
 CANDLE_MODEL_IMPL=app
 
-CSC249=/lus/grand/projects/CSC249ADOA01
-ROOT=$CSC249/public/sfw/polaris
-# SWIFT=$ROOT/swift-t/2023-06-05
-SWIFT=$ROOT/swift-t/2023-08-31
+# CSC249=/lus/grand/projects/CSC249ADOA01
+# ROOT=$CSC249/public/sfw/polaris
+# # SWIFT=$ROOT/swift-t/2023-06-05
+# SWIFT=$ROOT/swift-t/2023-08-31
+
+CANDLE_ECP=/eagle/Candle_ECP
+ROOT=$CANDLE_ECP/sfw
+SWIFT=$ROOT/swift-t/2024-03-13
 
 if ! [[ -d $SWIFT ]]
 then
@@ -19,13 +23,13 @@ export TURBINE_HOME=$SWIFT/turbine
 PATH=$SWIFT/stc/bin:$PATH
 PATH=$SWIFT/turbine/bin:$PATH
 
-PY=$ROOT/Miniconda-2023-06-16
+PY=$CANDLE_ECP/conda/2024-03-12
 
 PATH=$PY/bin:$PATH
 
-R_HOME=$ROOT/R-4.2.2/lib64/R
-EQR=$ROOT/EQ-R
+# R_HOME=$ROOT/R-4.2.2/lib64/R
+# EQR=$ROOT/EQ-R
 
 module load PrgEnv-nvhpc
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:$R_HOME/lib
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:$R_HOME/lib
