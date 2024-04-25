2fc7c68d72cb1c55e8461452cd364041d35afd99
Justin M Wozniak <wozniak@uan-0002.head.cm.americas.sgi.com>
2024-04-25 16:54:48 -0500

    Fix indent

--- workflows/common/sh/get-last-experiment.zsh
+++ workflows/common/sh/get-last-experiment.zsh
@@ -5,15 +5,16 @@
 D()
 # Find the latest experiment directory, assign to environment variable D
 {
-   D=( experiments/*(om[1]) ) ; d D
-   local _D
-   _D=$D
-   unset D
-   export D=$_D
+
+  D=( experiments/*(om[1]) ) ; d D
+  local _D
+  _D=$D
+  unset D
+  export D=$_D
 }
 
 E()
 # Inspect the outputs in $D
 {
-   e $D/output.txt $D/out/out-*.txt
+  e $D/output.txt $D/out/out-*.txt
 }
