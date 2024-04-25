500d261096702f586914385a7517e340a568664f
Justin M Wozniak <wozniak@uan-0002.head.cm.americas.sgi.com>
2024-04-25 15:46:09 -0500

    WIP on CANDLE_OUTPUT_DIRECTORY for new hyperparameter model_outdir

--- workflows/common/sh/model.sh
+++ workflows/common/sh/model.sh
@@ -58,29 +58,27 @@ then
   MODEL_TOKEN=$( basename $MODEL_NAME .sif )
   # The container will create subdirectories based on
   #               --experiment_id and --run_id
-  # This directory is bound inside the container:
-  export CANDLE_OUTPUT_DIR=/candle_data_dir/$MODEL_TOKEN/Output
   # This directory is outside the container:
-  RUN_DIRECTORY=$CANDLE_DATA_DIR/$MODEL_TOKEN/Output/$EXPID/$RUNID
-  mkdir -pv $RUN_DIRECTORY
+  CANDLE_OUTPUT_DIRECTORY=$CANDLE_DATA_DIR/$MODEL_TOKEN/Output/$EXPID/$RUNID
+  mkdir -pv $CANDLE_OUTPUT_DIRECTORY
 elif [[ $MODEL_TYPE == "BENCHMARKS" ]]
 then
-  RUN_DIRECTORY=$TURBINE_OUTPUT/$RUNID
-  mkdir -pv $RUN_DIRECTORY
+  CANDLE_OUTPUT_DIRECTORY=$TURBINE_OUTPUT/$RUNID
+  mkdir -pv $CANDLE_OUTPUT_DIRECTORY
   export CANDLE_OUTPUT_DIR=$( realpath --canonicalize-existing \
-                                       $RUN_DIRECTORY )
+                                       $CANDLE_OUTPUT_DIRECTORY )
 else
   echo "model.sh: Unknown model type: '$MODEL_TYPE'"
   exit 1
 fi
 
 # All stdout/stderr after this point goes into model.log !
-LOG_FILE=$RUN_DIRECTORY/model.log
+LOG_FILE=$CANDLE_OUTPUT_DIRECTORY/model.log
 echo "redirecting to: LOG_FILE=$LOG_FILE"
 set +x
 exec >> $LOG_FILE
 exec 2>&1
-cd $RUN_DIRECTORY
+cd $CANDLE_OUTPUT_DIRECTORY
 
 source $WORKFLOWS_ROOT/common/sh/utils.sh
 LOG_NAME="MODEL.SH"
@@ -140,18 +138,18 @@ then
 
   FLAGS=$( python3 $WORKFLOWS_ROOT/common/python/runner_utils.py expand_params \
                    "$PARAMS" )
-
+  FLAGS+=" --model_outdir $CANDLE_OUTPUT_DIRECTORY"
   # Remove --candle image flag and the second argument, assume it is the last argument
   export FLAGS="${FLAGS/ --candle_image*/}"
 
   # The Singularity command line arguments:
   MODEL_CMD=( singularity exec --nv
               --bind $CANDLE_DATA_DIR:/candle_data_dir
-              $MODEL_NAME ${MODEL_ACTION}.sh $CVD
+              --bind $CANDLE_OUTPUT_DIRECTORY
+              $MODEL_NAME ${MODEL_ACTION}.sh
+              $CVD
               /candle_data_dir
               $FLAGS
-              --experiment_id $EXPID
-              --run_id $RUNID
             )
 
 else # "BENCHMARKS"
@@ -159,7 +157,7 @@ else # "BENCHMARKS"
   # The Python command line arguments:
   PY_CMD=( "$WORKFLOWS_ROOT/common/python/model_runner.py"
            "$PARAMS"
-           "$RUN_DIRECTORY"
+           "$CANDLE_OUTPUT_DIRECTORY"
            "$CANDLE_FRAMEWORK"
            "$RUNID"
            "$BENCHMARK_TIMEOUT" )
@@ -189,23 +187,23 @@ log "$MODEL_TYPE: EXIT CODE: $CODE"
 if (( CODE == 0 ))
 then
   echo PWD: $( pwd -P )
-  echo RUN_DIRECTORY: $RUN_DIRECTORY
+  echo CANDLE_OUTPUT_DIRECTORY: $CANDLE_OUTPUT_DIRECTORY
   ls -ltrh
   sleep 1  # Wait for output
   # Get last results of the format "IMPROVE_RESULT LABEL 123.456" in model.log
   # The LABEL is optional, only the numeric substring will be extracted
   # NOTE: Enabling set -x will break the following (token CANDLE_RESULT)
   RES=$( awk -v FS="IMPROVE_RESULT" 'NF>1 {x=$2} END {print x}' \
-             $RUN_DIRECTORY/model.log )
+             $CANDLE_OUTPUT_DIRECTORY/model.log )
   RESULT="$(echo $RES | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')" || true
   echo "IMPROVE_RESULT: '$RESULT'"
-  echo $RESULT > $RUN_DIRECTORY/result.txt
+  echo $RESULT > $CANDLE_OUTPUT_DIRECTORY/result.txt
   if [[ ${RESULT_FILE:-} != "" ]]
   then
     echo $RESULT > $RESULT_FILE
   fi
   # Log hyperparameters and result
-  echo "${MODEL_CMD[@]}" > $RUN_DIRECTORY/run_command.txt # Store the model command
+  echo "${MODEL_CMD[@]}" > $CANDLE_OUTPUT_DIRECTORY/run_command.txt # Store the model command
   PARAMETERS=() # Initialize parameters
   # Store parameters from MODEL_CMD except for nv, bind, and experiment_id
   for i in "${!MODEL_CMD[@]}"; do
@@ -218,7 +216,7 @@ then
   done
   PARAMETERS+=("$RESULT") # Add the result as well
   # Print all values as comma-separated to the experiment directory output file
-  IFS=','; echo "${PARAMETERS[*]}" >> $RUN_DIRECTORY/../output.csv; unset IFS
+  IFS=','; echo "${PARAMETERS[*]}" >> $CANDLE_OUTPUT_DIRECTORY/../output.csv; unset IFS
 else
   echo # spacer
   if (( CODE == 124 ))
