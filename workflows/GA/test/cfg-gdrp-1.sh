9965534f05e436189d48f454771ddd05e7391dcd
Justin Wozniak <woz@anl.gov>
2024-05-08 13:11:32 -0500

    Latest startup example

--- workflows/GA/test/cfg-gdrp-1.sh
+++ workflows/GA/test/cfg-gdrp-1.sh
@@ -14,14 +14,37 @@ export CANDLE_FRAMEWORK="pytorch"
 
 # SMALL:
 export PROCS=3
-export POPULATION_SIZE=2
-export NUM_ITERATIONS=2
+export PPN=2
+export POPULATION_SIZE=4
+export NUM_ITERATIONS=3
+export WALLTIME=00:05:00
+export QUEUE=${QUEUE:-debug}
 
 # # MEDIUM:
-# export PROCS=4
-# export POPULATION_SIZE=16
+# export PROCS=16
+# export PPN=4
+# export POPULATION_SIZE=32
 # export NUM_ITERATIONS=4
+# export WALLTIME=00:20:00
+# export QUEUE=${QUEUE:-debug}
 
-# LARGE:
+# # LARGE:
+# export PROCS=400
+# export PPN=4
 # export POPULATION_SIZE=80
 # export NUM_ITERATIONS=10
+# export WALLTIME=24:00:00
+# export QUEUE=${QUEUE:-debug-scaling}
+
+export PYTHONPATH=$HOME/proj/IMPROVE
+export CANDLE_DATA_DIR=/eagle/Candle_ECP/wozniak/CDD
+export PROJECT=IMPROVE
+
+# Be sure to turn MPICH_GPU off!
+export TURBINE_PRELAUNCH="
+  module use /soft/modulefiles
+  module load conda
+  conda activate
+  source /lus/eagle/projects/Candle_ECP/conda/venv-2024-04-30/bin/activate
+  export MPICH_GPU_SUPPORT_ENABLED=0
+"
