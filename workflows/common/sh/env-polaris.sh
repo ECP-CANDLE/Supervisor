79392641ca5a92214a1e88fa07414491bf76946a
Justin Wozniak <woz@anl.gov>
2024-05-24 15:28:56 -0500

    Use new Swift/T and set TURBINE_WORKER_HOOK

--- workflows/common/sh/env-polaris.sh
+++ workflows/common/sh/env-polaris.sh
@@ -8,13 +8,21 @@ CANDLE_MODEL_IMPL=container
 
 CANDLE_ECP=/eagle/Candle_ECP
 ROOT=$CANDLE_ECP/sfw
-SWIFT=$ROOT/swift-t/2024-05-02
+SWIFT=$ROOT/swift-t/2024-05-23
 
 if ! [[ -d $SWIFT ]]
 then
   echo "Not found: SWIFT=$SWIFT"
   exit 1
 fi
+
+# Set CUDA_VISIBLE_DEVICES for CANDLE_MODEL_IMPL=py cases
+# using Turbine hook:
+export TURBINE_WORKER_HOOK_STARTUP='
+puts "TURBINE WORKER HOOK"
+set env(CUDA_VISIBLE_DEVICES) $env(ADLB_RANK_OFFSET)
+puts CUDA_VISIBLE_DEVICES=$env(CUDA_VISIBLE_DEVICES)
+'
 
 export TURBINE_HOME=$SWIFT/turbine
 PATH=$SWIFT/stc/bin:$PATH
