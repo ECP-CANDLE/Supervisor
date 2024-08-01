c753abe0483d2a118c46de48b07068005b7f3cef
Justin Wozniak <woz@anl.gov>
2024-08-01 15:12:47 -0500

    Works

--- scripts/random-upf2.py
+++ scripts/random-upf2.py
@@ -1,4 +1,9 @@
 
+"""
+RANDOM UPF
+See README or --help for usage
+"""
+
 import logging
 import random
 import sys
@@ -20,9 +25,7 @@ def main():
         exit(1)
 
 def parse_args():
-    """
-    Use argparse to handle command line
-    """
+    """ Use argparse to handle command line """
     import argparse
     parser = argparse.ArgumentParser(
         prog="random_upf",
@@ -33,6 +36,7 @@ def parse_args():
                         default=42,
                         help="random seed")
     parser.add_argument("count",
+                        type=int,
                         help="number of rows to generate")
     parser.add_argument("param_set_file",
                         help="the DEAP parameter set file")
@@ -42,15 +46,14 @@ def parse_args():
     return args
 
 
-
 class UserError(Exception):
-    """ Generic user error for the random_upf program. """
+    """ Generic user error for the program. """
     pass
 
 
 def setup_logger(logger):
     """ Set up the logger for development use """
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     h = logging.StreamHandler(stream=sys.stdout)
     fmtr = logging.Formatter(
             "%(asctime)s %(name)s %(levelname)-5s %(message)s",
@@ -68,8 +71,27 @@ def make_upf(logger, args):
         logger.info("PARAM: %i: %s" % (i+1, repr(param)))
     entries = []
     for i in range(0, args.count):
-        entries.append(get_entry(logger, args))
+        entry = get_entry(logger, args, ga_params)
+        entries.append(entry)
+    write_upf(logger, args, entries)
 
+
+def get_entry(logger, args, params):
+    values = deap_ga.draw_random(params)
+    # print("choice: " + str(values))
+    result = \
+        deap_ga.create_json_string_values(params, values, indent=2)
+    # print("result: " + result)
+    return result
+
+
+def write_upf(logger, args, entries):
+    filename = args.output
+    logger.info("write: " + filename)
+    with open(filename, "w") as fp:
+        for entry in entries:
+            fp.write(entry)
+            fp.write("\n")
 
 
 if __name__ == "__main__":
