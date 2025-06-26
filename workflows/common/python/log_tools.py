375ed6b4fa877d20cd974cb8fbf12133948c914e
Justin Wozniak <woz@anl.gov>
2025-06-26 14:41:04 -0500

    Reformat

--- workflows/common/python/log_tools.py
+++ workflows/common/python/log_tools.py
@@ -8,12 +8,16 @@ logger = None
 
 
 def get_logger(logger, name, stream=sys.stdout, milliseconds=False):
-    """Set up logging if necessary If the caller's logger already exists, just
-    return it."""
-    if logger is not None:
-        return logger
-    import logging
 
+    """
+    Set up logging if necessary
+    If the caller's logger already exists, just return it.
+    """
+
+    if logger is not None: return logger
+    
+    import logging
+    
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     h = logging.StreamHandler(stream=stream)
