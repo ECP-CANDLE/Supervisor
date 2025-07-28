b47261a4dac0e75ebd74f4e023febcb147ea4148
Justin Wozniak <woz@anl.gov>
2025-07-28 14:49:11 -0500

    Comment out container support

--- scripts/shrink-log.mk
+++ scripts/shrink-log.mk
@@ -3,8 +3,8 @@
 
 # Logs on stdout from Python runs
 OUTS = $(wildcard out-*.txt)
-# Logs in model.log from containers
-RUNS = $(shell find . -name model.log)
+# # Logs in model.log from containers
+# RUNS = $(shell find . -name model.log)
 
 SUMMARIES = $(subst out-,summary-,$(OUTS)) $(subst model,summary,$(RUNS))
 
